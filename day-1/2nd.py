1f98ed4974a75b7e1873fc99d5308e83e84b8f26
Oliver R Jahren <oliverrj@ifi.uio.no>
2021-12-04 20:35:43 +0100

    optimize 1 2

--- day-1/2nd.py
+++ day-1/2nd.py
@@ -1,15 +1,13 @@
 res = 0
 prev = None
-windows = []
 with open("input.txt", "r") as f:
     nums = [int(x) for x in f.readlines()]
 
 for i in range(0, len(nums) - 2, 1):
-    windows.append(sum([nums[i], nums[i + 1], nums[i + 2]]))
-
-for x in windows:
+    x = sum([nums[i], nums[i + 1], nums[i + 2]])
     if prev:
         if x > prev:
             res += 1
     prev = x
+
 print("res:", res)
