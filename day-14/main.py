0aa94159f6225b161db67faf531f71ca7929abd8
Oliver R Jahren <oliverrj@ifi.uio.no>
2021-12-14 13:43:21 +0100

    solve 14 1

--- day-14/main.py
+++ day-14/main.py
@@ -1,25 +1,39 @@
 from collections import defaultdict
+from collections import Counter
+from itertools import pairwise
+
 ls = []
 s = None
 dd = defaultdict(lambda x: False)
+cnt = defaultdict(int)
 with open("input.txt", "r") as f:
-    s = f.readline().strip()
+    s = f.readline().rstrip()
     f.readline()
     ls = [x.strip() for x in f.readlines()]
-    ls = [tuple(x.split(" -> ")) for x in ls]
+    ls = [tuple(x.split(" -> ", maxsplit=1)) for x in ls]
     for a, b in ls:
         dd[a] = b
 
-print(ls)
-print("s:", s)
-print(dd)
+res = []
+co = 10
+bis = defaultdict(int)
 
-ny = list(s)
+for c in s:
+    cnt[c] += 1
 
-for i, c in enumerate(s):
-    j = i + 1
-    if j < len(s):
-        a, b = s[i], s[j]
-        if dd[a + b]:
-            ny.insert(i, dd[a + b])
-print("".join(ny))
+for a, b in pairwise(s):    
+    bis[a + b] += 1
+
+for _ in range(10):
+    l = []
+    for k, v in ls:
+        if bis[k] != 0:
+            l.append((k, v, bis[k]))
+            del bis[k]
+    
+    for k, v, c in l:
+        cnt[v] += c
+        bis[k[0] + v] += c
+        bis[v + k[1]] += c
+ll = Counter(cnt).most_common()
+print("res:", ll[0][1] - ll[-1][1])
