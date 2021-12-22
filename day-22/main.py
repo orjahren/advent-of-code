c50c0426cdc7331a46fcba36aba73cfd982bcb6a
Oliver R Jahren <oliverrj@ifi.uio.no>
2021-12-22 16:37:13 +0100

    lol 22 1

--- day-22/main.py
+++ day-22/main.py
@@ -2,23 +2,13 @@ from collections import defaultdict
 import functools
 import operator
 
-class Cube:
-    def __init__(self, x, y, z) -> None:
-        self.x = x
-        self.y = y
-        self.z = z
-        self.on = False
-    
-    def set_on(self):
-        self.on = True
+dd = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: 0)))
 
-    def set_off(self):
-        self.on = False
+#0 = undefined, 1 = on, -1 = off
 
-all_cubes = []
-dd = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: False)))
 #with open("input.txt", "r") as f:
-with open("test.txt", "r") as f:
+#with open("test.txt", "r") as f:
+with open("storre.txt", "r") as f:
     for line in f.readlines():
         split = line.split(" ")
         cmd = split[0]
@@ -33,15 +23,19 @@ with open("test.txt", "r") as f:
             for y in range(y_range[0], y_range[1] + 1):
                 for z in range(z_range[0], z_range[1] + 1):
                     #print(x, y, z)
-                    a = dd[x][y][z]
-                    if a == False:
-                        ny = Cube(x, y, z)
-                        dd[x][y][z] = ny
-                        all_cubes.append(ny)
-                        a = ny
+                    
                     if cmd == "on":
-                        a.set_on()
+                        dd[x][y][z] = 1
                     else:
-                        a.set_off()
-res = len(list(filter(lambda x: x.on, all_cubes)))
+                        dd[x][y][z] = -1
+print("begnin") 
+res = 0
+for x in dd.values():
+    for y in x.values():
+        for z in y.values():
+            if z == 1:
+                res += 1
+        
+#all_cubes = dd.values().values().values()
+#res = len(list(filter(lambda x: x == 1, all_cubes)))
 print("res:", res)
