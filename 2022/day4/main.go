5e0da302d22d78466afc7e1ab1a64d607d299a09
Oliver Jahren <oliverrj@ifi.uio.no>
2022-12-04 11:30:50 +0100

    Cleanup

--- 2022/day4/main.go
+++ 2022/day4/main.go
@@ -14,29 +14,6 @@ func check(e error) {
 	}
 }
 
-func dealWithString(inp string, out chan int) {
-
-	spl := strings.Split(inp, ",")
-	spenn1 := strings.Split(spl[0], "-")
-	spenn2 := strings.Split(spl[1], "-")
-
-	a, _ := strconv.Atoi(spenn1[0])
-	b, _ := strconv.Atoi(spenn1[1])
-
-	x, _ := strconv.Atoi(spenn2[0])
-	y, _ := strconv.Atoi(spenn2[1])
-	// Spenn1 inneholder hele spenn2
-	if a <= x && b >= y {
-		out <- 1
-	} else if x <= a && y >= b {
-		// Spenn2 inneholder hele spenn1
-		out <- 1
-	} else {
-		out <- 0
-	}
-
-}
-
 func min(x, y int) int {
 	if x < y {
 		return x
@@ -56,7 +33,7 @@ func doesOverlap(p, q, r, s int) bool {
 	return max(p, r) <= min(q, s)
 }
 
-func part2(inp string, out chan int) {
+func dealWithString(inp string, part1 chan int, part2 chan int) {
 
 	spl := strings.Split(inp, ",")
 	spenn1 := strings.Split(spl[0], "-")
@@ -67,12 +44,21 @@ func part2(inp string, out chan int) {
 
 	x, _ := strconv.Atoi(spenn2[0])
 	y, _ := strconv.Atoi(spenn2[1])
-	if doesOverlap(a, b, x, y) || doesOverlap(x, y, a, b) {
-		out <- 1
+	// Spenn1 inneholder hele spenn2
+	if a <= x && b >= y {
+		part1 <- 1
+	} else if x <= a && y >= b {
+		// Spenn2 inneholder hele spenn1
+		part1 <- 1
 	} else {
-		out <- 0
+		part1 <- 0
 	}
 
+	if doesOverlap(a, b, x, y) || doesOverlap(x, y, a, b) {
+		part2 <- 1
+	} else {
+		part2 <- 0
+	}
 }
 
 func main() {
@@ -81,35 +67,26 @@ func main() {
 	check(err)
 	defer f.Close()
 	scanner := bufio.NewScanner(f)
-	isOverlapping := make(chan int)
+	completeOverlap := make(chan int)
+
 	anyOverlap := make(chan int)
 
 	numberOfBlocks := 0
 	for scanner.Scan() {
-		text := scanner.Text()
-		go dealWithString(text, isOverlapping)
-		go part2(text, anyOverlap)
+		go dealWithString(scanner.Text(), completeOverlap, anyOverlap)
 		numberOfBlocks++
 	}
 	readBlocks := 0
-	res := 0
+	part1Res := 0
+	part2Res := 0
 	for true {
-		res += <-isOverlapping
+		part1Res += <-completeOverlap
+		part2Res += <-anyOverlap
 		readBlocks++
 		if readBlocks == numberOfBlocks {
 			break
 		}
 	}
-	fmt.Println("Part 1: ", res)
-	readBlocks = 0
-	res = 0
-	for true {
-		res += <-anyOverlap
-		readBlocks++
-		if readBlocks == numberOfBlocks {
-			break
-		}
-	}
-
-	fmt.Println("Part 2: ", res)
+	fmt.Println("Part 1: ", part1Res)
+	fmt.Println("Part 2: ", part2Res)
 }
