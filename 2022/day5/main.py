a4d48c20f8bd02f7656c64b4865ab1a61b9651fa
Oliver Jahren <oliverrj@ifi.uio.no>
2022-12-05 12:28:17 +0100

    Dont use list of ops

--- 2022/day5/main.py
+++ 2022/day5/main.py
@@ -1,6 +1,4 @@
-stacks = [[] for _ in range(9)]  # manuell minne-allokering??
-ops = []
-# with open("small", "r") as f:
+stacks = [[] for _ in range(9)]
 with open("input", "r") as f:
     ll = f.readlines()
     lager_stax = True
@@ -8,21 +6,15 @@ with open("input", "r") as f:
         spl = l.split()
         if len(spl) == 0:
             lager_stax = False
-            continue
-        if lager_stax:
+        elif lager_stax:
             # this is so stupid lol
             for lix, i in enumerate(range(1, len(l), 4)):
                 if l[i].strip():
                     stacks[lix].append(l[i])
         else:
-            ops.append(spl)
-
-while len(ops) > 0:
-    cmd = ops.pop(0)
-    fra = stacks[int(cmd[3]) - 1]
-    til = stacks[int(cmd[5]) - 1]
-
-    for i in range(int(cmd[1])):
-        til.insert(i, fra.pop(0))
+            fra = stacks[int(spl[3]) - 1]
+            til = stacks[int(spl[5]) - 1]
+            for i in range(int(spl[1])):
+                til.insert(i, fra.pop(0))
 
 print("Part 2:", "".join([x[0] for x in stacks if x]))
