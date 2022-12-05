d42162ad6cfbcb4e8982217364c7beedcba492b2
Oliver Jahren <oliverrj@ifi.uio.no>
2022-12-05 12:56:27 +0100

    Use fewer lines

--- 2022/day5/main.py
+++ 2022/day5/main.py
@@ -1,19 +1,12 @@
-stacks = [[] for _ in range(9)]
 with open("input", "r") as f:
-    lager_stax = True
+    stacks = [[] for _ in range(9)]
     for l in f.readlines():
-        spl = l.split()
-        if len(spl) == 0:
-            lager_stax = False
-        elif lager_stax:
-            # this is so stupid lol
+        if l[0] == "m":
+            spl = l.split()
+            for i in range(int(spl[1])):
+                stacks[int(spl[5]) - 1].insert(i, stacks[int(spl[3]) - 1].pop(0))
+        else:
             for lix, i in enumerate(range(1, len(l), 4)):
                 if l[i].strip():
                     stacks[lix].append(l[i])
-        else:
-            fra = stacks[int(spl[3]) - 1]
-            til = stacks[int(spl[5]) - 1]
-            for i in range(int(spl[1])):
-                til.insert(i, fra.pop(0))
-
 print("Part 2:", "".join([x[0] for x in stacks if x]))
