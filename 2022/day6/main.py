4282908faefe1365fd07c0f57a43ea10c87ede3e
Oliver Jahren <oliverrj@ifi.uio.no>
2022-12-06 09:06:01 +0100

    Use fewer lines

--- 2022/day6/main.py
+++ 2022/day6/main.py
@@ -1,8 +1,5 @@
-# with open("small", "r") as f:
 with open("input", "r") as f:
     buf = f.readline()
-    for i, s in enumerate(buf):
-        if i > 13:
-            if len(set(buf[i - 14 : i])) == 14:
-                print("Part 1:", i)
-                # exit()
+    for i in range(len(buf)):
+        if i > 13 and len(set(buf[i - 14 : i])) == 14:
+            print("Part 2:", i)
