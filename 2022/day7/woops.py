9119568580ce5a981a861df70d854abf7182a35a
Oliver Jahren <oliverrj@ifi.uio.no>
2022-12-07 16:31:19 +0100

    Ridiculosuly braindadly solve 7 1

--- 2022/day7/woops.py
+++ 2022/day7/woops.py
@@ -2,38 +2,73 @@ from collections import defaultdict
 
 
 class File:
-    def __init__(self) -> None:
+    def __init__(self, abs_path, size) -> None:
         self.name = None
-        self.size = None
+        self.size = int(size)
         self.parent = None
+        self.path = abs_path
 
 
 class Folder:
-    def __init__(self):
-        self.name
+    def __init__(self, abs_path):
         self.files = []
-        self.pa
+        self.parent = None
+        self.path = abs_path
+        self.subfolders = []
+
+    def __str__(self) -> str:
+        return (
+            "<Folder med "
+            + str(len(self.subfolders))
+            + " subfoldre og "
+            + str(len(self.files))
+            + " filer. Dennes size: "
+            + str(self.get_size())
+            + " >"
+        )
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    def get_size(self):
+        print("Skal finne size for " + self.path)
+        tot = 0
+        for folder in self.subfolders:
+            subfolder_value = folder.get_size()
+            print("adder", subfolder_value, "til tot fra subfolder", str(folder))
+            tot += subfolder_value
+        for file in self.files:
+            print("adder ", file.size, "fra fil")
+            tot += file.size
+        return tot
 
 
 def get_path(path):
-    return "/" + "/".join(path[1:])
+    print("Skal finne abs_path for", path)
+    if len(path) == 1 or len(path) == 0:
+        return "/"
+    # jelif len(path) == 2:
+    # return "/" + path[1]
+    else:
+        return "/" + "/".join(path)[1:]
 
 
 files = []
 ll = []
-with open("small", "r") as f:
+# with open("small", "r") as f:
+with open("input", "r") as f:
     ll = [x.strip() for x in f.readlines()]
-# print(ll)
 
 dd = defaultdict(lambda: 0)
 
-fs = {}
+fs = {"/": Folder("/")}
+all_folders = [fs["/"]]
 
 path = []
 for l in ll:
 
-    # print("Leste dette:", l)
-    # print("Current path er:", get_path(path))
+    print("Leste dette:", l)
+    print("Current path er:", get_path(path))
 
     spl = l.split()
     if spl[0] == "$":
@@ -50,83 +85,40 @@ for l in ll:
             # print("Skal fikse fs for", path, ", som oversettes til", get_path(path))
             print("Skal legge", spl[1], "inn i fs under mappe", get_path(path), ". Path er:", path)
             # for p in get_path(lie)
-
-            prev = fs
-            for p in path:
-                print("Sjekker ut", p)
-                parent = prev.get(p, None)
-                if parent is None:
-                    print(p, "finnes ikke")
-                    print("i prev:", prev)
-                    prev[p] = {}
-                    prev[p][spl[1]] = {}
-                else:
-                    print(p, "finnes")
-                    prev = prev[p]
-                    if p == path[-1]:
-                        print(p, "er", path[-1])
-                        print("prevv er:", prev)
-                        prev[spl[1]] = {}
+            glob_path = get_path(path)
+            # fol = fs.get(glob_path, Folder(glob_path))
+            parent_folder = fs.get(glob_path)  # , Folder(glob_path))
+            print("Har funnet parent folder:", parent_folder, "på path", glob_path)
+            new_path = glob_path + "/" + spl[1]
+            new_folder = Folder(new_path)
+            parent_folder.subfolders.append(new_folder)
+            print("Lagret denne folderen til path:", new_path)
+            fs[new_path] = new_folder
+            all_folders.append(new_folder)
 
         else:
             # ls
             print("Skal lager størrelsen av", spl[1], "til", path)
             print("Den str er", spl[0])
             parsedNumber = int(spl[0])
-            # dd[get_path(path)] += parsedNumber
-            d = fs["/"]
-            for p in path[1:]:
-                d = d[p]
-            print("d er:", d)
-            if not d.get("files", False):
-                print(d, "er ikke ei liste")
-                d["files"] = []
-
-            d["files"].append({spl[1]: parsedNumber})
-            # d[spl[1]] = parsedNumber  # <<<- save file name with file size at proper place
-            # d[parsedNumber] = False  # <<<<- hack the world
-            if not d.get("META"):
-                d["META"] = 0
-
-            d["META"] += parsedNumber
+            abs_path = get_path(path)
+            folder = fs[abs_path]
+            file_path = abs_path + "/" + spl[1]
+            folder.files.append(File(file_path, spl[0]))
 
 
-print("Ferdig fs:")
+print("Antall folders:", len(all_folders))
+print(all_folders)
+
+print("Fs:")
 print(fs)
+# print(fs["//a"].get_size())
 
 
-k = "/"
-q = list(fs[k].keys())
-prev = fs
-# for k, v in fs.items():
-cnt = 0
-while len(q) > 0:
-    print("Setter x til k av y", prev, k)
-    x = prev.get(k, None)
-    print("Sjekker ut:", x)
-
-    if x:
-        # if type(x) == dict:
-        # if list(x.values())[0] != False:new
-        next_q = [v for v in list(x.keys()) if str(v) not in ["META", "files"]]
-        print("Legger dette i q:", next_q)
-        q.extend(next_q)
-        # else:
-        # print(x, "er ikke en dict ;)")
-        prev = x
-
-    else:
-        print("x er none, så dette må være et løv-dir")
-        print(prev)
-        prev = prev
-
-    # prev = x
-    k = q.pop()
-"""
+roof = 100000
 res = 0
-for k, v in fs.items():
-    print("Sjekker:", k, v)
-    if v < 100000:
-        res += v
+for folder in all_folders:
+    size = folder.get_size()
+    if size < roof:
+        res += size
 print("Part 1:", res)
-"""
