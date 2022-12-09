986991b0bb373d2503342ef423e9e798423d271e
Oliver Jahren <oliverrj@ifi.uio.no>
2022-12-09 14:16:42 +0100

    Optimize

--- 2022/day9/main.py
+++ 2022/day9/main.py
@@ -1,13 +1,6 @@
 ll = [(s[0], int(s[1])) for x in open("input").readlines() if (s := x.split())]
-
-
-def get_tup(head, old_tail):
-    return head[0] - old_tail[0], head[1] - old_tail[1]
-
-
 head, tail = [0, 0], [0, 0]
 visited = set()
-
 for act, val in ll:
     for _ in range(val):
         match (act):
@@ -19,15 +12,14 @@ for act, val in ll:
                 head[0] += 1
             case "L":
                 head[0] -= 1
-        old_tail = tuple(tail)
-
-        if get_tup(head, old_tail) in [(-2, 1), (-1, 2), (0, 2), (1, 2), (2, 1)]:
+        tup = head[0] - tail[0], head[1] - tail[1]
+        if tup in [(-2, 1), (-1, 2), (0, 2), (1, 2), (2, 1)]:
             tail[1] += 1
-        if get_tup(head, old_tail) in [(1, 2), (2, 1), (2, 0), (2, -1), (1, -2)]:
+        if tup in [(1, 2), (2, 1), (2, 0), (2, -1), (1, -2)]:
             tail[0] += 1
-        if get_tup(head, old_tail) in [(2, -1), (1, -2), (0, -2), (-1, -2), (-2, -1)]:
+        if tup in [(2, -1), (1, -2), (0, -2), (-1, -2), (-2, -1)]:
             tail[1] -= 1
-        if get_tup(head, old_tail) in [(-1, -2), (-2, -1), (-2, -0), (-2, 1), (-1, 2)]:
+        if tup in [(-1, -2), (-2, -1), (-2, -0), (-2, 1), (-1, 2)]:
             tail[0] -= 1
         visited.add(tuple(tail))
 print("Part 1:", len(visited))
