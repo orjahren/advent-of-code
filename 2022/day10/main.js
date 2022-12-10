26ef292c3258cf0650fe7a700b4831af78eb7085
Oliver Jahren <oliverrj@ifi.uio.no>
2022-12-10 18:49:04 +0100

    Solve 10 2 in TS

--- 2022/day10/main.js
+++ 2022/day10/main.js
@@ -1,6 +1,5 @@
 "use strict";
 "use scrict";
-var _a;
 const fs = require("fs");
 const lines = fs.readFileSync("input", "utf8").split("\n");
 const doPart1 = (currentCycle, reg) => currentCycle == 20 ||
@@ -9,34 +8,54 @@ const doPart1 = (currentCycle, reg) => currentCycle == 20 ||
     currentCycle == 140 ||
     currentCycle == 180 ||
     currentCycle == 220
-    ? curretCycle * reg
+    ? currentCycle * reg
     : 0;
-let curretCycle = 0;
-let reg = 1;
-let part1Res = 0;
-let horIdx = 0;
-let pop1 = 0;
-let pop2 = 0;
-let line = "";
-let done = false;
-while (!done) {
-    curretCycle++;
-    part1Res += doPart1(curretCycle, reg);
-    pop1 = pop2;
-    pop2 = 0;
-    if (pop1 !== 0) {
-        reg += pop1;
+const drawCRT = (reg, horIdx) => {
+    if (reg - 1 <= horIdx && reg + 1 >= horIdx) {
+        process.stdout.write("█");
     }
     else {
-        line = (_a = lines.shift()) !== null && _a !== void 0 ? _a : " ";
+        process.stdout.write(" ");
     }
-    if (line[0] === "a") {
-        pop2 = parseInt(line.split(" ")[1]);
+};
+(() => {
+    var _a;
+    let currentCycle = 0;
+    let reg = 1;
+    let part1Res = 0;
+    let pop1 = 0;
+    let pop2 = 0;
+    let horIdx = 0;
+    let line = "";
+    let done = false;
+    while (!done) {
+        currentCycle++;
+        part1Res += doPart1(currentCycle, reg);
+        drawCRT(reg, horIdx);
+        pop1 = pop2;
+        pop2 = 0;
+        if (pop1 !== 0) {
+            reg += pop1;
+        }
+        else {
+            line = (_a = lines.shift()) !== null && _a !== void 0 ? _a : " ";
+        }
+        if (line[0] === "a") {
+            pop2 = parseInt(line.split(" ")[1]);
+        }
+        else if (!(pop1 != 0 || pop2 != 0 || line[0] === "n")) {
+            done = true;
+            break;
+        }
+        line = " ";
+        (() => {
+            horIdx++;
+            if (horIdx == 40) {
+                horIdx = 0;
+                //process.stdout.wri
+                console.log("");
+            }
+        })();
     }
-    else if (!(pop1 != 0 || pop2 != 0 || line[0] === "n")) {
-        done = true;
-        break;
-    }
-    line = " ";
-}
-console.log("Part 1: ", part1Res);
+    console.log("Part 1: ", part1Res);
+})();
--- 2022/day10/main.ts
+++ 2022/day10/main.ts
@@ -8,33 +8,50 @@ const doPart1 = (currentCycle: number, reg: number): number =>
   currentCycle == 140 ||
   currentCycle == 180 ||
   currentCycle == 220
-    ? curretCycle * reg
+    ? currentCycle * reg
     : 0;
 
-let curretCycle = 0;
-let reg = 1;
-let part1Res = 0;
-let horIdx = 0;
-let pop1: number = 0;
-let pop2: number = 0;
-let line = "";
-let done = false;
-while (!done) {
-  curretCycle++;
-  part1Res += doPart1(curretCycle, reg);
-  pop1 = pop2;
-  pop2 = 0;
-  if (pop1 !== 0) {
-    reg += pop1;
+const drawCRT = (reg: number, horIdx: number) => {
+  if (reg - 1 <= horIdx && reg + 1 >= horIdx) {
+    process.stdout.write("█");
   } else {
-    line = lines.shift() ?? " ";
+    process.stdout.write(" ");
   }
-  if (line[0] === "a") {
-    pop2 = parseInt(line.split(" ")[1]);
-  } else if (!(pop1 != 0 || pop2 != 0 || line[0] === "n")) {
-    done = true;
-    break;
+};
+(() => {
+  let currentCycle = 0;
+  let reg = 1;
+  let part1Res = 0;
+  let pop1: number = 0;
+  let pop2: number = 0;
+  let horIdx = 0;
+  let line = "";
+  let done = false;
+  while (!done) {
+    currentCycle++;
+    part1Res += doPart1(currentCycle, reg);
+    drawCRT(reg, horIdx);
+    pop1 = pop2;
+    pop2 = 0;
+    if (pop1 !== 0) {
+      reg += pop1;
+    } else {
+      line = lines.shift() ?? " ";
+    }
+    if (line[0] === "a") {
+      pop2 = parseInt(line.split(" ")[1]);
+    } else if (!(pop1 != 0 || pop2 != 0 || line[0] === "n")) {
+      done = true;
+      break;
+    }
+    line = " ";
+    (() => {
+      horIdx++;
+      if (horIdx == 40) {
+        horIdx = 0;
+        console.log("");
+      }
+    })();
   }
-  line = " ";
-}
-console.log("Part 1: ", part1Res);
+  console.log("Part 1: ", part1Res);
+})();
