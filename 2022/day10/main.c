b17410333eb1dae5983694b1551d95b78d68e7a5
Oliver Jahren <oliverrj@ifi.uio.no>
2022-12-10 13:42:44 +0100

    Solve 10 2

--- 2022/day10/main.c
+++ 2022/day10/main.c
@@ -23,29 +23,53 @@ int main()
     int pendingOperation2 = __INT_MAX__;
     int nopFlag = 0;
     int done = 0;
+    int horIdx = 0;
     while (done == 0)
     {
-        printf("************* Starter cycle %d, reg er %d \n", currentCycle, reg);
+        // printf("************* Starter cycle %d, reg er %d \n", currentCycle, reg);
         if (currentCycle == 20 || currentCycle == 60 || currentCycle == 100 || currentCycle == 140 || currentCycle == 180 || currentCycle == 220)
         {
-            printf("Current cycle er %d og reg_value er %d (som sammen utgjør %d) \n", currentCycle, reg, currentCycle * reg);
+            // printf("Current cycle er %d og reg_value er %d (som sammen utgjør %d) \n", currentCycle, reg, currentCycle * reg);
             res += currentCycle * reg;
         }
+        //  TEGN HER
+        // printf("SKal tegne reg %d", reg);
+        if (reg - 1 <= horIdx && reg + 1 >= horIdx)
+        {
 
-        printf("Flytter 2 til 1 \n");
+            printf("#");
+            // printf("#%d", currentCycle);
+        }
+        else
+        {
+
+            printf(".");
+            // printf(".%d", currentCycle);
+        }
+
+        horIdx++;
+        if (horIdx == 40)
+        {
+            printf("  NEWLINE. cuycCount: %d  ", currentCycle);
+            printf("\n");
+            horIdx = 0;
+        }
+        // Tegn slutt
+
+        // printf("Flytter 2 til 1 \n");
         pendingOperation1 = pendingOperation2;
-        printf("Pend op 1 er %d \n", pendingOperation1);
+        // printf("Pend op 1 er %d \n", pendingOperation1);
 
         if (pendingOperation1 != __INT_MAX__)
         {
-            printf("Skal inkrementere reg med %d \n", pendingOperation1);
+            // printf("Skal inkrementere reg med %d \n", pendingOperation1);
             reg += pendingOperation1;
         }
         else
         {
-            printf("LESER \n");
+            // printf("LESER \n");
             fgets(line, 10, fp);
-            printf("Lests: %s", line);
+            // printf("Lests: %s", line);
         }
         pendingOperation2 = __INT_MAX__;
 
@@ -54,7 +78,7 @@ int main()
             int val;
             char devNull[10];
             sscanf(line, "%s %d", &devNull, &val);
-            printf("Setter pend op 2 til: %d \n", val);
+            // printf("Setter pend op 2 til: %d \n", val);
             pendingOperation2 = val;
         }
         else if (line[0] == 'n')
@@ -64,12 +88,12 @@ int main()
 
         if (!((nopFlag > 0) || (pendingOperation1 != __INT_MAX__) || (pendingOperation2 != __INT_MAX__)))
         {
-            printf("Avslutter. \n");
-            done = 1;
+            // printf("Avslutter. \n");
+            done = 1; // Kan fjernes pga break
             break;
         }
         nopFlag = 0;
-        printf("---------------- Avslutter cycle %d, reg er %d \n \n", currentCycle, reg);
+        // printf("---------------- Avslutter cycle %d, reg er %d \n \n", currentCycle, reg);
         currentCycle++;
         memset(line, 0, sizeof(char) * 10);
     }
@@ -77,5 +101,7 @@ int main()
     printf("Final reg_value er %d\n", reg);
     printf("Part 1: %d \n", res);
     printf("Final cycle_value er %d\n", currentCycle);
+
+    printf("Part 2: Se over... \n");
     return 0;
 }
