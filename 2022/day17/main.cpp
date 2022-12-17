6602e078385826e3e7d965780aa4b1a4d70c65dc
Oliver Jahren <oliverrj@ifi.uio.no>
2022-12-17 13:19:28 +0100

    Solve 17 1 in c++

--- 2022/day17/main.cpp
+++ 2022/day17/main.cpp
@@ -3,176 +3,103 @@
 #include <fstream>
 #include <vector>
 #include <complex>
+#include <unordered_map>
+#include <map>
 
-#define LOG(x) std::cout << x << std::endl;
-
-class Rock
-{
-
-public:
-    char formation[4][4];
-    short rockCount;
-    Rock()
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            for (int j = 0; j < 4; j++)
-            {
-                this->formation[i][j] = '.';
-            }
-        }
-    }
-    void fall(char direction, char grid[7][1000])
-    {
-    }
-    void lock()
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            for (int j = 0; j < 4; j++)
-            {
-                char c = this->formation[i][j];
-                if (c == '#')
-                {
-                    this->formation[i][j] = '#';
-                }
-            }
-        }
-    }
-    void draw()
-    {
-
-        LOG("Skal tegne en stein")
-        for (int i = 0; i < 4; i++)
-        {
-            for (int j = 0; j < 4; j++)
-            {
-                char c = this->formation[i][j];
-                std::cout << c;
-            }
-            std::cout << std::endl;
-        }
-    }
-};
-
-Rock makeRock(int type)
-{
-    LOG("SKal lage en rock av type " << type)
-    Rock r;
-    // Flat strek bortover
-    if (type == 1)
-    {
-        for (short i = 0; i < 4; i++)
-        {
-            r.formation[0][i] = '@';
-        }
-        r.rockCount = 4;
-
-    } // pluss
-    else if (type == 2)
-    {
-        // Topp
-        r.formation[0][1] = '#';
-        // Vertikal rad
-        for (short i = 0; i < 3; i++)
-        {
-            r.formation[1][i] = '@';
-        }
-        // Bunn
-        r.formation[2][1] = '@';
-        r.rockCount = 5;
-
-    } // Speilvendt l
-    else if (type == 3)
-    {
-        // Nedover
-        for (short i = 0; i < 3; i++)
-        {
-            r.formation[i][2] = '@';
-        }
-        // Bortover nederst
-        for (short i = 0; i < 2; i++)
-        {
-            r.formation[2][i] = '@';
-        }
-        r.rockCount = 5;
-    }
-    // Rett strek nedover
-    else if (type == 4)
-    {
-        // Nedover
-        for (short i = 0; i < 5; i++)
-        {
-            r.formation[i][0] = '@';
-            ;
-        }
-        r.rockCount = 4;
-    } // kompakt firkant
-    else if (type == 5)
-    {
-        for (short i = 0; i < 2; i++)
-        {
-            for (short j = 0; j < 2; j++)
-            {
-                r.formation[i][j] = '@';
-            }
-        }
-        r.rockCount = 4;
-    }
-    return r;
-}
-void drawGrid(char grid[7][1000])
-{
-    for (int i = 0; i < 1000; i++)
-    {
-        for (int j = 0; j < 7; j++)
-        {
-            char c = grid[j][i];
-            std::cout << c;
-        }
-        std::cout << std::endl;
-    }
-}
 int main()
 {
     std::cout << "Hello world \n";
-    char grid[7][1000];
-    for (int i = 0; i < 1000; i++)
-    {
-        for (int j = 0; j < 7; j++)
-        {
-            grid[j][i] = '.';
-        }
-    }
-    drawGrid(grid);
-    Rock rockTypes[5];
-    /*
-    for (int i = 1; i < 6; i++)
-    {
-
-        Rock r = makeRock(i);
-        r.draw();
-    }
-    */
-    std::ifstream file("small");
+    // std::ifstream file("small");
+    std::ifstream file("input");
     std::string line;
     getline(file, line);
     file.close();
-    LOG(line)
-    int operationIndex = 0;
-    int stoppedRocks = 0;
-    int rockCounter = 0;
-    bool isFalling = false;
-    // for (int iteration = 1; iteration < 2022; iteration++)
-    while (rockCounter < 2022)
+    int windSize = line.size();
+    // Use bits to indicate rock occupancy
+    std::vector<int> rocks[5] = {{0x1E}, {0x08, 0x1C, 0x08}, {0x1C, 0x04, 0x04}, {0x10, 0x10, 0x10, 0x10}, {0x18, 0x18}};
+
+    int side[5][2] = {{-2, 1}, {-2, 2}, {-2, 2}, {-2, 4}, {-2, 3}};
+
+    std::vector<int> field;
+    int sp = 0;
+    int lineIdx = 0;
+
+    field.push_back(0x7F);
+
+    auto stonefit = [&](int rock, int shift, int bp) -> bool
     {
-        if (!isFalling)
+        for (int r = 0; r < rocks[rock].size(); r++)
         {
-            isFalling = true;
-            rockCounter++;
-            Rock r = makeRock(rockCounter % 5 == 0 ? 5 : rockCounter % 5);
-            // int val = rockCounter % 5 == 0 ? 5 : rockCounter % 5;
-            // std::cout << val << std::endl;
+            int sv = shift > 0 ? rocks[rock][r] >> shift : rocks[rock][r] << -shift;
+            if (bp + r < field.size() && (field[bp + r] & sv))
+                return false;
+        }
+        return true;
+    };
+
+    std::vector<int> heights;
+    std::map<std::pair<int, int>, std::vector<int>> drops;
+    std::map<int, int> loopCount;
+
+    while (true)
+    {
+        int bp = field.size() + 3;
+        int shift = 0;
+        bool drop = true;
+        do
+        {
+            // Blow the wind
+            switch (line[lineIdx])
+            {
+            case '<':
+                if (shift != side[sp % 5][0] && stonefit(sp % 5, shift - 1, bp))
+                {
+                    shift--;
+                }
+                break;
+            case '>':
+                if (shift != side[sp % 5][1] && stonefit(sp % 5, shift + 1, bp))
+                {
+                    shift++;
+                }
+                break;
+            }
+
+            lineIdx = (lineIdx + 1) % windSize;
+            drop = stonefit(sp % 5, shift, bp - 1);
+            if (drop)
+            {
+                --bp;
+            }
+        } while (drop);
+        int top = bp + rocks[sp % 5].size();
+        if (field.size() < top)
+        {
+            field.resize(top);
+        }
+        for (int r = 0; r < rocks[sp % 5].size(); r++)
+        {
+            int sv = shift > 0 ? rocks[sp % 5][r] >> shift : rocks[sp % 5][r] << -shift;
+            field[bp + r] |= sv;
+        }
+        ++sp;
+        heights.push_back(field.size() - 1);
+
+        auto check = std::make_pair((sp - 1) % 5, (lineIdx - 1 + windSize) % windSize);
+        if (drops[check].size() != 0)
+        {
+            for (int x : drops[check])
+            {
+                int d = (sp - 1) - x;
+                loopCount[d]++;
+            }
+        }
+        drops[check].push_back(sp - 1);
+        if (sp == 2022)
+        {
+            std::cout << "Part 1: " << field.size() - 1 << std::endl;
+            return 0;
         }
     }
+    return 0;
 }
---
+++ 2022/readme.md
@@ -0,0 +1,1 @@
+The bitvector strat is not mine, insipred by https://old.reddit.com/r/adventofcode/comments/znykq2/2022_day_17_solutions/j0k7szh/ , https://github.com/progheal/adventofcode/blob/master/2022/17.cpp
