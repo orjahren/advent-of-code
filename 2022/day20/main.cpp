01cdda246bffed8aadbd202fec1cde59bc7614a7
Oliver Jahren <oliverrj@ifi.uio.no>
2022-12-20 23:45:01 +0100

    stash

--- 2022/day20/main.cpp
+++ 2022/day20/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <vector>
 #include <utility>
+#include <algorithm>
 
 #define LOG(x) std::cout << x << std::endl;
 
@@ -37,16 +38,15 @@ int main()
     std::cout << "Begynner simulering..\n";
     for (int i = 0; i < pairs.size(); i++)
     {
-        std::pair<int, int> p = pairs.at(i);
+        std::pair<int, int> &p = pairs.at(i);
         // int newIdx = (((i + p.second) % pairs.size()) + pairs.size()) % pairs.size(); // burde ikke gjøre denne beregningen hvis casen under slår inn
+        // int newIdx = (((p.first + p.second) % pairs.size()) + pairs.size()) % pairs.size(); // burde ikke gjøre denne beregningen hvis casen under slår inn
         int newIdx = (((p.first + p.second) % pairs.size()) + pairs.size()) % pairs.size(); // burde ikke gjøre denne beregningen hvis casen under slår inn
-        /*
-        if (i + p.second < 0)
+        if (i + p.second <= 0)
         {
             newIdx = pairs.size() + p.second; // subtraher fra slutten
             LOG("Hacket ny idx til: " << newIdx)
         }
-        */
 
         if (newIdx < 0)
         {
@@ -54,15 +54,39 @@ int main()
             return 1;
         }
         LOG("\t Skal flytte " << p.second << " fra " << p.first << " til " << newIdx)
+        std::cout << "Før: \t";
+        printArr(arr, pairs.size());
+        /*
+        arr.insert(arr.begin() + newIdx, p.second);
+        arr.erase(arr.begin() + p.first);
+        */
         arr.erase(arr.begin() + p.first);
         arr.insert(arr.begin() + newIdx, p.second);
+
         p.first = newIdx;
+        std::cout << "Etter: \t";
         printArr(arr, pairs.size());
 
         LOG("Oppdaterer indexer")
         for (int j = 0; j < pairs.size(); j++)
         {
-            pairs.at(j).first = j;
+            // pairs.at(j).first = j;
+            auto idx = std::find(arr.begin(), arr.end(), pairs.at(j).second);
+            if (idx != arr.end())
+            {
+                std::cout << "Fant " << *idx << " på " << idx - arr.begin() << std::endl;
+                pairs.at(j).first = idx - arr.begin();
+            }
+            else
+            {
+                LOG("Fant ikke " << pairs.at(j).second << " i arr...");
+                return 1;
+            }
+            //  pairs.at(j).first = arr.begin(), arr.end(), pairs.at(j).second);
+            /*
+            std::pair<int, int> &p = pairs.at(j);
+            arr.at(j) = p.second;
+            */
         }
     }
     LOG("...done")
