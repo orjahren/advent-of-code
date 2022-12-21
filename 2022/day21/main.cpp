c4ffc1e49f9e91b298cda54752ce068ccce22daf
Oliver Jahren <oliverrj@ifi.uio.no>
2022-12-21 15:55:19 +0100

    Solve 21 1 in c++

--- 2022/day21/main.cpp
+++ 2022/day21/main.cpp
@@ -3,8 +3,6 @@
 #include <fstream>
 #include <deque>
 
-#define LOG(x) std::cout << x << std::endl;
-
 double useCharAsOp(double lx, char op, double rx)
 {
     switch (op)
@@ -17,11 +15,6 @@ double useCharAsOp(double lx, char op, double rx)
         return lx - rx;
     case '*':
         return lx * rx;
-
-    default:
-        LOG("FANT IKKE CHAR")
-        exit(1);
-        break;
     }
 }
 
@@ -41,7 +34,6 @@ int main()
 {
     std::unordered_map<std::string, double> hm;
     std::string line;
-    // std::ifstream file("small");
     std::ifstream file("input");
     std::deque<std::string> q;
 
@@ -55,36 +47,22 @@ int main()
         std::string l = q.front(); // TODO: Burde være en referanse? Må poppe senere i så fall
         std::string orig = q.front();
         q.pop_front();
-        LOG("L er: " << l)
         std::size_t cIdx = l.find(": ");
-        LOG(cIdx)
         std::string var = l.substr(0, cIdx);
         std::string exp = l.substr(cIdx + 2);
-        LOG(exp)
-        LOG(var)
         if (isNumber(exp))
         {
             hm[var] = std::stoi(exp);
-            LOG("\t\tConverterte " << exp << " til " << hm[var])
         }
         else
         {
-            LOG("Exp som skal parses: " << exp)
-            // Anta alle variabler har len(x) == 4
             std::string lop = exp.substr(0, 4);
             char op = exp.at(5);
             std::string rop = exp.substr(7);
-            LOG("Lop op rop: " << lop << op << rop)
-
-            if (hm.count(lop) && hm.count(rop))
+            if (hm.count(lop) > 0 && hm.count(rop) > 0)
             {
                 double lx = hm[lop];
                 double rx = hm[rop];
-                if (var == "root")
-                {
-                    LOG(lx)
-                    LOG(rx)
-                }
                 hm[var] = useCharAsOp(lx, op, rx);
             }
             else
@@ -93,5 +71,5 @@ int main()
             }
         }
     }
-    std::cout << "Part 1: " << hm["root"] << std::endl;
+    std::cout << "Part 1: " << std::setprecision(100) << hm["root"] << std::endl;
 }
