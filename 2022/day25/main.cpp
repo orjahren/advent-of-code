b0c86e5c37f5f5f3097301c904fe20de59b3b4d6
Oliver Jahren <oliverrj@ifi.uio.no>
2022-12-26 22:32:22 +0100

    Optimize

--- 2022/day25/main.cpp
+++ 2022/day25/main.cpp
@@ -1,35 +1,9 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <vector>
-#include <math.h>
 #include <deque>
 
-#define LOG(x) std::cout << x << std::endl;
 using ll = long long;
-
-std::string convertToSNAFU(ll val)
-{
-    std::string str = std::to_string(val);
-    std::deque<char> q;
-    ll tmp = val;
-    std::string chars("=-012");
-    while (tmp != 0)
-    {
-        ll div = floor((tmp + 2) / 5);
-        int idx = (tmp + 2) % 5;
-        tmp = div;
-        q.push_front(chars.at(idx));
-    }
-    std::string res;
-    while (q.size() > 0)
-    {
-        char c = q.front();
-        res.push_back(c);
-        q.pop_front();
-    }
-    return res;
-}
 
 ll convertToInt(std::string snafu)
 {
@@ -37,8 +11,7 @@ ll convertToInt(std::string snafu)
     for (int i = snafu.size() - 1; i >= 0; i--)
     {
         char c = snafu.at(i);
-        int pos = snafu.size() - i - 1;
-        int val = c - '0';
+        int val;
         if (c == '-')
         {
             val = -1;
@@ -47,30 +20,43 @@ ll convertToInt(std::string snafu)
         {
             val = -2;
         }
-        ll cal = val * pow(5, pos);
-        res += cal;
+        else
+        {
+            val = c - '0';
+        }
+        ll tmp = 1;
+        for (int j = 0; j < snafu.size() - i - 1; j++)
+        {
+            tmp *= 5;
+        }
+        res += val * tmp;
     }
     return res;
 }
 
 int main()
 {
-    std::cout << "Hello world" << std::endl;
     std::string line;
-    // std::ifstream file("small");
     std::ifstream file("input");
-
-    std::vector<int> nums;
     ll toBeConv = 0;
     while (getline(file, line))
     {
-        std::cout << line << " -> ";
-        ll num = convertToInt(line);
-        nums.push_back(num);
-        std::cout << num << std::endl;
-        toBeConv += num;
+        toBeConv += convertToInt(line);
     }
-    LOG("To be converted to SNAFU: " << toBeConv)
-    std::string conv = convertToSNAFU(toBeConv);
-    LOG("Part 1: " << conv)
+    std::string str = std::to_string(toBeConv);
+    std::deque<char> q;
+    char chars[5] = {'=', '-', '0', '1', '2'};
+    std::string res;
+    while (toBeConv != 0)
+    {
+        ll pf = toBeConv + 2;
+        toBeConv = pf / 5;
+        q.push_front(chars[pf % 5]);
+    }
+    while (q.size() > 0)
+    {
+        res.push_back(q.front());
+        q.pop_front();
+    }
+    std::cout << "Part 1: " << res << std::endl;
 }
