1d237ecc72a4819e23cc67c01160a8b1be8a25f1
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-12-01 08:13:56 +0100

    Solve example.

--- 2023/day01/main.hs
+++ 2023/day01/main.hs
@@ -1,2 +1,31 @@
-main = do 
-    putStrLn "Salut le monde!"
+getFirstNumber = take 1
+
+getLastNumber = take 1 . reverse
+
+-- concatNumbers :: Char -> Char -> String
+-- concatNumbers x y = show x ++ show y
+concatNumbers x y = x ++ y
+
+symbolIsNumber = (`elem` "0123456789")
+
+main = do
+  content <- readFile "example"
+  let lines = words content
+  -- extract only the numbers from every line
+  let runes = map (filter symbolIsNumber) lines
+
+  -- get first and last number from every line
+  let firstNumbers = map getFirstNumber runes
+  let lastNumbers = map getLastNumber runes
+
+  -- concat first and last number
+  let runes = zipWith concatNumbers firstNumbers lastNumbers
+
+  let nums = map read runes :: [Int]
+
+  putStrLn content
+  print runes
+  print nums
+
+  let res = sum nums
+  print res
