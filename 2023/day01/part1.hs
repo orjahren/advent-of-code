831317b45c7c5e43721587c811d28c0781162ad5
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-12-02 10:39:47 +0100

    273 chars

--- 2023/day01/part1.hs
+++ 2023/day01/part1.hs
@@ -1,10 +1,7 @@
 concatNumbers x y = x ++ y
 
-symbolIsNumber = (`elem` "0123456789")
-
 main = do
-  content <- readFile "example"
-  let runes = map (filter symbolIsNumber) (words content)
+  content <- readFile "input"
+  let runes = map (filter (`elem` "123456789")) (words content)
   let numsAsString = zipWith concatNumbers (map (take 1) runes) (map (take 1 . reverse) runes)
-  let nums = map read numsAsString :: [Int]
-  print (sum nums)
+  print (sum (map read numsAsString :: [Int]))
