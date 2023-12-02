3f2e9c69c68a9e9741fff2af9f173ff5c1aedc53
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-12-02 13:13:25 +0100

    More cleanup

--- 2023/day02/main.ts
+++ 2023/day02/main.ts
@@ -3,8 +3,8 @@ import * as fs from "fs";
 const part1 = (fileName: string): number => {
   const getIdIfValid = (game: string): number => {
     const rgb = [0, 0, 0];
-    const gameSpl = game.split(":");
-    return gameSpl[1].split(";").every((bit) => {
+    const [idStr, content] = game.split(":");
+    return content.split(";").every((bit) => {
       bit
         .split(",")
         .map((x) => x.trim())
@@ -14,7 +14,7 @@ const part1 = (fileName: string): number => {
         });
       return rgb[0] < 13 && rgb[1] < 14 && rgb[2] < 15;
     })
-      ? parseInt(gameSpl[0].split(" ")[1])
+      ? parseInt(idStr.substring(5))
       : 0;
   };
   return fs
@@ -30,14 +30,16 @@ const part2 = (fileName: string): number => {
     game
       .split(":")[1]
       .split(";")
-      .forEach((bit) => {
-        const split = bit.split(",").map((x) => x.trim());
-        split.forEach((op) => {
-          const [value, color] = op.split(" ");
-          const idx = ["red", "green", "blue"].indexOf(color);
-          rgb[idx] = Math.max(parseInt(value), rgb[idx]);
-        });
-      });
+      .forEach((bit) =>
+        bit
+          .split(",")
+          .map((x) => x.trim())
+          .forEach((op) => {
+            const [value, color] = op.split(" ");
+            const idx = ["red", "green", "blue"].indexOf(color);
+            rgb[idx] = Math.max(parseInt(value), rgb[idx]);
+          })
+      );
     return rgb.reduce((acc, val) => acc * val, 1);
   };
   return fs
