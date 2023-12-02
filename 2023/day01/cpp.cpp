18e391a4ab79ed3b223f1551e99eb14740748477
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-12-02 16:29:36 +0100

    Prepare for separate part2 file.

--- 2023/day01/cpp.cpp
+++ 2023/day01/cpp.cpp
@@ -5,13 +5,30 @@ bool charIsNum(char &c)
     return c >= '0' && c <= '9';
 }
 
+int stringToNum(string &s, vector<string> &nums)
+{
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (s == nums[i])
+            return i + 1;
+    }
+    return -1;
+}
+
+bool stringIsNumber(string &s, vector<string> &nums)
+{
+    if (s.size() == 0)
+        return charIsNum(s[0]);
+    return stringToNum(s, nums) != -1;
+}
+
 int main()
 {
     vector<string> nums = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
     string s;
-    int partOne, x, y;
+    int partOne, partTwo, x, y;
     char a, b;
-    partOne = 0;
+    partOne = partTwo = 0;
     while (cin >> s)
     {
         cout << s << endl;
