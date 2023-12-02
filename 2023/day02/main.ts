9c0d1ba2a685e06cce4a142f18f268cca96e952d
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-12-02 13:04:06 +0100

    Cleanup.

--- 2023/day02/main.ts
+++ 2023/day02/main.ts
@@ -1,75 +1,56 @@
 import * as fs from "fs";
 
 const part1 = (fileName: string): number => {
-  const LIMITS = {
-    red: 12,
-    green: 13,
-    blue: 14,
-  };
-
-  const gameIsValid = (game: string): boolean => {
-    const bits = game.split(":")[1].split(";");
+  const getIdIfValid = (game: string): number => {
     const rgb = [0, 0, 0];
-    return bits.every((bit) => {
-      const split = bit.split(",").map((x) => x.trim());
-      split.forEach((op) => {
-        const [value, color] = op.split(" ");
-        rgb[["red", "green", "blue"].indexOf(color)] = parseInt(value);
-      });
-      if (
-        rgb[0] > LIMITS.red ||
-        rgb[1] > LIMITS.green ||
-        rgb[2] > LIMITS.blue
-      ) {
-        return false;
-      }
-      return true;
-    });
+    const gameSpl = game.split(":");
+    return gameSpl[1].split(";").every((bit) => {
+      bit
+        .split(",")
+        .map((x) => x.trim())
+        .forEach((op) => {
+          const [value, color] = op.split(" ");
+          rgb[["red", "green", "blue"].indexOf(color)] = parseInt(value);
+        });
+      return rgb[0] < 13 && rgb[1] < 14 && rgb[2] < 15;
+    })
+      ? parseInt(gameSpl[0].split(" ")[1])
+      : 0;
   };
-
-  // TODO: Muligens mer smooth å bare returnere id fra valid, 0 hvis ikke valid
-  const getIdFromLine = (line: string): number => {
-    const x = line.split(":")[0];
-    return parseInt(x.split(" ")[1]);
-  };
-
-  const lines = fs.readFileSync(fileName, "utf-8").split("\n").filter(Boolean);
-
-  return lines.reduce((acc, line) => {
-    if (gameIsValid(line)) {
-      return acc + getIdFromLine(line);
-    }
-    return acc;
-  }, 0);
+  return fs
+    .readFileSync(fileName, "utf-8")
+    .split("\n")
+    .filter(Boolean)
+    .reduce((acc, line) => getIdIfValid(line) + acc, 0);
 };
+
 const part2 = (fileName: string): number => {
   const getPowerOfGame = (game: string): number => {
-    const bits = game.split(":")[1].split(";");
     const rgb = [0, 0, 0];
-    bits.forEach((bit) => {
-      const split = bit.split(",").map((x) => x.trim());
-      split.forEach((op) => {
-        const [value, color] = op.split(" ");
-        const idx = ["red", "green", "blue"].indexOf(color);
-        const parsed = parseInt(value);
-        rgb[idx] = Math.max(parsed, rgb[idx]);
+    game
+      .split(":")[1]
+      .split(";")
+      .forEach((bit) => {
+        const split = bit.split(",").map((x) => x.trim());
+        split.forEach((op) => {
+          const [value, color] = op.split(" ");
+          const idx = ["red", "green", "blue"].indexOf(color);
+          rgb[idx] = Math.max(parseInt(value), rgb[idx]);
+        });
       });
-    });
     return rgb.reduce((acc, val) => acc * val, 1);
   };
-  const lines = fs.readFileSync(fileName, "utf-8").split("\n").filter(Boolean);
-  return lines.reduce((acc, line) => acc + getPowerOfGame(line), 0);
+  return fs
+    .readFileSync(fileName, "utf-8")
+    .split("\n")
+    .filter(Boolean)
+    .reduce((acc, line) => getPowerOfGame(line) + acc, 0);
 };
 
 console.log("Part 1:");
 console.log(part1("example"));
 console.log(part1("input"));
-// Første resultat: 227. For lavt
-// Andre res: 128. (Også for lavt (lol))
-// Trede res: 2151. (Også for lavt??)
-// Fjerde res: 2239. Riktig
 
 console.log("Part 2:");
 console.log(part2("example"));
 console.log(part2("input"));
-// Res: 83435. Riktig på første forsøk
