6063942e99719f260873ade98fd2b531373cf7fa
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-12-03 17:16:23 +0100

    Cleanup part 1.

--- 2023/day03/cpp.cpp
+++ 2023/day03/cpp.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 struct numEnt
 {
-    int numVal;
-    string stringVal;
+    string val;
     short idxStart;
     short idxEnd;
     int lineIdx;
@@ -17,56 +16,30 @@ bool numberIsAdjacentToSymbol(numEnt num, vector<string> lines)
     {
         for (int idx = num.idxStart; idx <= num.idxEnd; idx++)
         {
-            // To the left of curr num
             if (num.idxStart > 0 && lines[num.lineIdx][idx - 1] == symbol)
-            {
                 return true;
-            }
-            // To the right on same line
             if (num.idxEnd < lines[num.lineIdx].size() - 1 && lines[num.lineIdx][idx + 1] == symbol)
-            {
                 return true;
-            }
 
-            // check diagonally
             if (num.lineIdx > 0)
             {
-                // top left
                 if (num.idxStart > 0 && lines[num.lineIdx - 1][idx - 1] == symbol)
-                {
                     return true;
-                }
-                // top right
                 if (num.idxEnd < lines[num.lineIdx - 1].size() - 1 && lines[num.lineIdx - 1][idx + 1] == symbol)
-                {
                     return true;
-                }
             }
 
             if (num.lineIdx < lines.size() - 1)
             {
-                // bottom left
                 if (num.idxStart > 0 && lines[num.lineIdx + 1][idx - 1] == symbol)
-                {
                     return true;
-                }
-                // bottom right
                 if (num.idxEnd < lines[num.lineIdx + 1].size() - 1 && lines[num.lineIdx + 1][idx + 1] == symbol)
-                {
                     return true;
-                }
             }
-            // check above and under
-            // above
             if (num.lineIdx > 0 && lines[num.lineIdx - 1][idx] == symbol)
-            {
                 return true;
-            }
-            // under
             if (num.lineIdx < lines.size() - 1 && lines[num.lineIdx + 1][idx] == symbol)
-            {
                 return true;
-            }
         }
     }
     return false;
@@ -81,11 +54,9 @@ int main()
 {
     string s;
     vector<numEnt> nums;
-    int lineCounter = 0;
     vector<string> lines;
-    while (cin >> s && ++lineCounter)
+    while (cin >> s)
     {
-        cout << s << endl;
         lines.push_back(s);
         for (int i = 0; i < s.size(); i++)
         {
@@ -93,44 +64,20 @@ int main()
             {
                 numEnt num;
                 num.idxStart = i;
-                num.lineIdx = lineCounter - 1;
-                cout << s[i] << " Es un numero" << endl;
+                num.lineIdx = lines.size() - 1;
                 int j = i;
                 while (j < s.size() && charIsNumeric(s[j]))
-                {
                     j++;
-                }
                 num.idxEnd = j - 1;
-                cout << "Found num from " << num.idxStart << " to " << num.idxEnd << endl;
                 string numStr = s.substr(num.idxStart, num.idxEnd - num.idxStart + 1);
-                cout << "NumStr: " << numStr << endl;
-                int parsed = stoi(numStr);
-                cout << "Parsed: " << parsed << endl;
-                num.stringVal = numStr;
-                num.numVal = parsed;
+                num.val = numStr;
                 nums.push_back(num);
-
                 i = j;
             }
         }
     }
-    cout << "Found " << nums.size() << " numbers" << endl;
     int res = 0;
     for (numEnt num : nums)
-    {
-        if (numberIsAdjacentToSymbol(num, lines))
-        {
-            cout << "Found valid num " << num.numVal << " at line " << num.lineIdx << endl;
-            res += num.numVal;
-        }
-        else
-        {
-            cout << "\t Not valid num " << num.numVal << " at line " << num.lineIdx << endl;
-        }
-    }
+        res += numberIsAdjacentToSymbol(num, lines) ? stoi(num.val) : 0;
     cout << "Part 1: " << res << endl;
 }
-// First res: 410371. Too low
-// 2nd res    492709. Also too low.
-// 3rd res    516469. Also too low.
-// 4th res    536576. Correct
