38c36e67b5769ea2d2699915bc7ed506c94078d8
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-12-04 18:15:28 +0100

    Cleanup.

--- 2023/day04/part1.hs
+++ 2023/day04/part1.hs
@@ -4,11 +4,8 @@ substringWithoutCard = takeWhileEnd (/= ':')
 
 splitOndSpace = map (splitOn (pack " "))
 
-getNumsOfSecondListThatAppearsInFirst :: [Int] -> [Int] -> [Int]
-getNumsOfSecondListThatAppearsInFirst xs ys = [y | x <- xs, y <- ys, x == y]
-
-getPairwiseOverlapp :: [[Int]] -> [Int]
-getPairwiseOverlapp = foldl1 getNumsOfSecondListThatAppearsInFirst
+getNumbersThatAppearsInBothLists :: [Int] -> [Int] -> [Int]
+getNumbersThatAppearsInBothLists xs ys = [x | x <- xs, y <- ys, x == y]
 
 getScoreRec :: [Int] -> Int -> Int
 getScoreRec [] acc = acc
@@ -20,20 +17,18 @@ getScore [x] = 1
 getScore arr = getScoreRec (tail arr) 1
 
 main = do
-  contents <- readFile "example"
-  -- contents <- readFile "input"
+  -- contents <- readFile "example"
+  contents <- readFile "input"
   let input = lines contents
-  let packedPipe = pack "|"
-  let bits = map (splitOn packedPipe . pack) input
+  let bits = map (splitOn (pack "|") . pack) input
   let bitsWithoutCard = map (map substringWithoutCard) bits
   let stripped = map (map strip) bitsWithoutCard
   let numsAsStr = map splitOndSpace stripped
   let unpacked = map (map (map unpack)) numsAsStr
   let filtered = map (map (filter (/= ""))) unpacked
   let nums = map (map (map (read :: String -> Int))) filtered
-  let overlapp = map getPairwiseOverlapp nums
+  let overlapp = map (foldl1 getNumbersThatAppearsInBothLists) nums
   let scores = map getScore overlapp
 
-  let finalResult = sum scores
   putStrLn "Part 1:"
-  print finalResult
+  print (sum scores)
