0d3c8e205ea9b60c1afd8ff32e09893d559f8af9
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-12-04 18:08:47 +0100

    Un-debug part 1.

--- 2023/day04/part1.hs
+++ 2023/day04/part1.hs
@@ -1,4 +1,4 @@
-import Data.Text (Text, isInfixOf, pack, replace, splitOn, strip, takeWhileEnd, unpack)
+import Data.Text (Text, pack, splitOn, strip, takeWhileEnd, unpack)
 
 substringWithoutCard = takeWhileEnd (/= ':')
 
@@ -20,43 +20,19 @@ getScore [x] = 1
 getScore arr = getScoreRec (tail arr) 1
 
 main = do
-  -- contents <- readFile "example"
-  contents <- readFile "input"
+  contents <- readFile "example"
+  -- contents <- readFile "input"
   let input = lines contents
-  print input
   let packedPipe = pack "|"
   let bits = map (splitOn packedPipe . pack) input
-  print bits
   let bitsWithoutCard = map (map substringWithoutCard) bits
-  putStrLn "bitsWithoutCard"
-  print bitsWithoutCard
   let stripped = map (map strip) bitsWithoutCard
-  putStrLn "stripped"
-  print stripped
-
-  putStrLn "numsAsStr"
   let numsAsStr = map splitOndSpace stripped
-  print numsAsStr
-
-  putStrLn "unpacked"
   let unpacked = map (map (map unpack)) numsAsStr
-  print unpacked
-
   let filtered = map (map (filter (/= ""))) unpacked
-  putStrLn "filtered"
-  print filtered
-
   let nums = map (map (map (read :: String -> Int))) filtered
-  putStrLn "nums"
-  print nums
-
-  putStrLn "overlapp"
   let overlapp = map getPairwiseOverlapp nums
-  print overlapp
-
   let scores = map getScore overlapp
-  putStrLn "Scores"
-  print scores
 
   let finalResult = sum scores
   putStrLn "Part 1:"
