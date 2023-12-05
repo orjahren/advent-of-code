49cb5b8d3b09b4f711dc45be8b804d88eb0d1a11
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-12-05 19:41:51 +0100

    Read and structure input.

--- 2023/day05/part1.go
+++ 2023/day05/part1.go
@@ -4,11 +4,23 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func stringContainsMap(s string) bool {
 	return strings.Contains(s, "map")
+}
+
+type mapEntry struct {
+	rangeStart int
+	rangeEnd   int
+	rangeSpan  int
+}
+
+type mapWrapper struct {
+	entries []mapEntry
+	name    string
 }
 
 func main() {
@@ -19,16 +31,47 @@ func main() {
 	stripped := strings.TrimSpace(line)
 	fmt.Println(stripped)
 
-	allLines := make([]string, 0)
+	fmt.Println("Starting loop.")
+	currMap := make([]mapEntry, 0)
+	allMaps := make([][]mapEntry, 0)
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			break
 		}
 		stripped := strings.TrimSpace(line)
-		fmt.Println(stripped)
-		allLines = append(allLines, stripped)
+		if stringContainsMap(stripped) {
+			if len(currMap) > 1 {
+				fmt.Println("currMap: ", currMap)
+				allMaps = append(allMaps, currMap)
+				fmt.Println("has length: ", len(currMap))
+			}
+			currMap = make([]mapEntry, 0)
+			continue
+		} else {
+			spl := strings.Split(stripped, " ")
+			fmt.Println("spl: ", spl)
+			if len(spl) < 3 {
+				continue
+			}
+			parr := make([]int, 3)
+
+			for i := range spl {
+				spl[i] = strings.TrimSpace(spl[i])
+				parsed, _ := strconv.Atoi(spl[i])
+				parr[i] = parsed
+			}
+			var me mapEntry
+			me.rangeStart = parr[0]
+			me.rangeEnd = parr[1]
+			me.rangeSpan = parr[2]
+
+			currMap = append(currMap, me)
+			fmt.Println(stripped)
+
+		}
 	}
-	fmt.Println(allLines)
-	fmt.Println(len(allLines))
+	allMaps = append(allMaps, currMap)
+	fmt.Println(allMaps)
+	fmt.Println(len(allMaps))
 }
