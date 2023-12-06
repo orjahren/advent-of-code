80afcf2554536c1f13076d20f9a681eeae196020
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-12-06 20:26:09 +0100

    Consolidate.

--- 2023/day06/part2.cpp
+++ 2023/day06/part2.cpp
@@ -1,44 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-bool optionWouldWin(ll opt, ll competitor, ll timeLimit)
-{
-    return (opt * (timeLimit - opt)) > competitor;
-}
-
 int howManyWinningOptions(ll timeLimit, ll competitor)
 {
     int res = 0;
     for (ll i = 0; i < timeLimit; i++)
-    {
-        if (optionWouldWin(i, competitor, timeLimit))
+        if ((i * (timeLimit - i)) > competitor)
             res++;
-    }
     return res;
 }
-
 int main()
 {
-    vector<string> v;
-    string inp;
-    cin >> inp;
-
     bool isTest = false;
     int numsToRead = isTest ? 3 : 4;
-
+    string inp;
+    cin >> inp;
+    vector<string> v(numsToRead);
     for (int i = 0; i < numsToRead; i++)
-    {
-        cin >> inp;
-        v.push_back(inp);
-    }
+        cin >> v[i];
     cin >> inp;
     string time = accumulate(v.begin(), v.end(), string(""));
-    v.clear();
     for (int i = 0; i < numsToRead; i++)
-    {
-        cin >> inp;
-        v.push_back(inp);
-    }
-    string dist = accumulate(v.begin(), v.end(), string(""));
-    cout << "Part 2: " << howManyWinningOptions(stoll(time), stoll(dist)) << endl;
+        cin >> v[i];
+    cout << "Part 2: " << howManyWinningOptions(stoll(time), stoll(accumulate(v.begin(), v.end(), string("")))) << endl;
 }
