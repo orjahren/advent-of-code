f8e1aa158f72c43ac93efdbaee1c8e1f8cc65f98
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-12-06 19:08:21 +0100

    Read input.

--- 2023/day06/part1.cpp
+++ 2023/day06/part1.cpp
@@ -1,5 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     cout << "Hello World!" << endl;
+    vector<int> times(3);
+    vector<int> distances(3);
+    string devNull;
+    cin >> devNull;
+    for (int i = 0; i < 3; i++)
+        cin >> times[i];
+    cin >> devNull;
+    for (int i = 0; i < 3; i++)
+        cin >> distances[i];
+    cout << "Times: ";
+    for (int i = 0; i < 3; i++)
+        cout << times[i] << " ";
+    cout << endl;
+    cout << "Distances: ";
+    for (int i = 0; i < 3; i++)
+        cout << distances[i] << " ";
+    cout << endl;
+    return 0;
 }
