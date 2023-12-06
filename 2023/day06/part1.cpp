9ce16c8672336beaad02e1a41e77860136aba7d4
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-12-06 20:00:32 +0100

    Cleanup.

--- 2023/day06/part1.cpp
+++ 2023/day06/part1.cpp
@@ -26,50 +26,30 @@ vector<int> getWinningOptions(int timeLimit, int competitor)
 
 int main()
 {
-    cout << "Hello World!" << endl;
     vector<int> times;
     vector<int> distances;
     string devNull;
     cin >> devNull;
     int x;
+
     bool isTest = false;
     int numsToRead = isTest ? 3 : 4;
+
     for (int i = 0; i < numsToRead; i++)
     {
         cin >> x;
         times.push_back(x);
     }
-
     cin >> devNull;
-
     for (int i = 0; i < numsToRead; i++)
     {
         cin >> x;
         distances.push_back(x);
     }
-    cout << "Times: ";
-    for (int i = 0; i < numsToRead; i++)
-        cout << times[i] << " ";
-    cout << endl;
-    cout << "Distances: ";
-    for (int i = 0; i < numsToRead; i++)
-        cout << distances[i] << " ";
-    cout << endl;
 
-    // int vals[3][3];
-    vector<vector<int>> vals(numsToRead, vector<int>(numsToRead));
     int res = 1;
     for (int i = 0; i < numsToRead; i++)
-    {
-        cout << "i: " << i << ", would win with " << endl;
-        vector<int> winningOptions = getWinningOptions(times[i], distances[i]);
-        vals[i] = winningOptions;
+        res *= getWinningOptions(times[i], distances[i]).size();
 
-        for (int j = 0; j < winningOptions.size(); j++)
-            cout << winningOptions[j] << " ";
-        cout << endl
-             << "i.e. " << vals[i].size() << " options" << endl;
-        res *= vals[i].size();
-    }
     cout << "Part 1: " << res << endl;
 }
