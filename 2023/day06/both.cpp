6d3b8e050d7f5d366c78fb6bbbaa5e4d171bd7d2
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-12-06 20:42:12 +0100

    Solve both parts in shared file.

--- 2023/day06/both.cpp
+++ 2023/day06/both.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
+typedef long long ll;
 using namespace std;
-int nWinningOptions(int timeLimit, int competitor)
+int nWinningOptions(ll timeLimit, ll competitor)
 {
     int res = 0;
     for (int i = 1; i < timeLimit; i++)
@@ -10,19 +11,22 @@ int nWinningOptions(int timeLimit, int competitor)
 }
 int main()
 {
-    string devNull;
-    cin >> devNull;
-    int x, res = 1;
     bool isTest = false;
     int numsToRead = isTest ? 3 : 4;
-    vector<int> times(numsToRead);
+    int x, part1 = 1;
+    string inp;
+    cin >> inp;
+    vector<string> v(numsToRead);
     for (int i = 0; i < numsToRead; i++)
-        cin >> times[i];
-    cin >> devNull;
+        cin >> v[i];
+    string time = accumulate(v.begin(), v.end(), string(""));
+    cin >> inp;
     for (int i = 0; i < numsToRead; i++)
     {
-        cin >> x;
-        res *= nWinningOptions(times[i], x);
+        cin >> inp;
+        part1 *= nWinningOptions(stoll(v[i]), stoll(inp));
+        v[i] = inp;
     }
-    cout << "Part 1: " << res << endl;
+    cout << "Part 1: " << part1 << endl;
+    cout << "Part 2: " << nWinningOptions(stoll(time), stoll(accumulate(v.begin(), v.end(), string("")))) << endl;
 }
