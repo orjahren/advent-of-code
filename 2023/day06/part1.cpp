0c0c7832df706e7d0144a614945dc6e93986d4b1
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-12-06 19:48:32 +0100

    Solve example in C++.

--- 2023/day06/part1.cpp
+++ 2023/day06/part1.cpp
@@ -1,5 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
+bool optionWouldWin(int opt, int competitor, int timeLimit)
+{
+    int currentSpeedInMs = 0;
+    int speedIncreasesByMsPrSec = 1;
+    // TODO: There must be some way to do this in O(1) time lol
+    // Set currSped = opt and no loop?
+    for (int i = 0; i < opt; i++)
+    {
+        currentSpeedInMs += speedIncreasesByMsPrSec;
+    }
+    return (currentSpeedInMs * (timeLimit - opt)) > competitor;
+}
+
+vector<int> getWinningOptions(int timeLimit, int competitor)
+{
+    vector<int> winningOptions;
+    for (int i = 0; i < timeLimit; i++)
+    {
+        if (optionWouldWin(i, competitor, timeLimit))
+            winningOptions.push_back(i);
+    }
+    return winningOptions;
+}
+
 int main()
 {
     cout << "Hello World!" << endl;
@@ -20,5 +44,21 @@ int main()
     for (int i = 0; i < 3; i++)
         cout << distances[i] << " ";
     cout << endl;
-    return 0;
+
+    // int vals[3][3];
+    vector<vector<int>> vals(3, vector<int>(3));
+    int res = 1;
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "i: " << i << ", would win with " << endl;
+        vector<int> winningOptions = getWinningOptions(times[i], distances[i]);
+        vals[i] = winningOptions;
+
+        for (int j = 0; j < winningOptions.size(); j++)
+            cout << winningOptions[j] << " ";
+        cout << endl
+             << "i.e. " << vals[i].size() << " options" << endl;
+        res *= vals[i].size();
+    }
+    cout << "Part 1: " << res << endl;
 }
