1a95562101b5bc40cf5d8cea3d8311eb47907976
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-12-07 14:18:27 +0100

    Vague outline.

--- 2023/day07/part1.cpp
+++ 2023/day07/part1.cpp
@@ -1,5 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
+enum HandEnum
+{
+    five = 7,
+    four = 6,
+    house = 5,
+    three = 4,
+    twoPairs = 3,
+    onePair = 2,
+    highCard = 1
+};
+
+int getEnumForHand(string hand)
+{
+    if (hand == "five-of-a-kind")
+    {
+        return five;
+    }
+    else if (hand == "four-of-a-kind")
+    {
+        return four;
+    }
+    else if (hand == "full-house")
+    {
+        return house;
+    }
+    else if (hand == "three-of-a-kind")
+    {
+        return three;
+    }
+    else if (hand == "two-pairs")
+    {
+        return twoPairs;
+    }
+    else if (hand == "pair")
+    {
+        return onePair;
+    }
+    else
+    {
+        return highCard;
+    }
+}
+
 int main()
 {
+    string hand;
+    int val;
+    while (cin >> hand)
+    {
+        cin >> val;
+        cout << hand << " " << val << endl;
+    }
 }
