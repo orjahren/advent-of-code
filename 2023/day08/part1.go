4e1dd05502afa3df30f4dca47be00c589654cc5e
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-12-08 15:04:54 +0100

    Procedure for input handling.

--- 2023/day08/part1.go
+++ 2023/day08/part1.go
@@ -22,40 +22,30 @@ type Node struct {
 	nameOfRight string
 }
 
-func main() {
-	//file, _ := os.Open("input")
-	file, _ := os.Open("example")
-	reader := bufio.NewReader(file)
-
-	instructions, _ := reader.ReadString('\n')
-	fmt.Println(instructions)
-	reader.ReadString('\n')
-
-	lines := make([]string, 0)
+func readFileAndStructureInput(reader bufio.Reader) ([]Node, map[string]*Node) {
 	nodes := make([]Node, 0)
 	nameToNodeMap := make(map[string]*Node)
 
 	onlyLettersRegex := regexp.MustCompile("[a-zA-Z]+")
-
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			break
 		}
-		lines = append(lines, line)
 		fmt.Println("Spl: ")
 		spl := strings.Split(line, " ")
 		fmt.Println(len(spl))
 		if len(spl) > 1 {
-
 			var node Node
 			fmt.Println(spl)
 			for i, s := range spl {
 				fmt.Println(i, ": "+s)
 			}
-			node.name = spl[0]
-			node.nameOfLeft = onlyLettersRegex.FindString(spl[2])
-			node.nameOfRight = onlyLettersRegex.FindString(spl[3])
+			letters := onlyLettersRegex.FindAllString(line, -1)
+			fmt.Println("Letters: ", letters)
+			node.name = letters[0]
+			node.nameOfLeft = letters[1]
+			node.nameOfRight = letters[2]
 			fmt.Println(node)
 			nodes = append(nodes, node)
 			nameToNodeMap[node.name] = &node
@@ -68,9 +58,24 @@ func main() {
 	for i, node := range nodes {
 		node.left = nameToNodeMap[node.nameOfLeft]
 		node.right = nameToNodeMap[node.nameOfRight]
+		// TODO: fører dette til noe wack med at noden får ny minneadresse?
 		nodes[i] = node
 	}
 	fmt.Println("\t Har satt opp pekere")
+	return nodes, nameToNodeMap
+}
+
+func main() {
+	//file, _ := os.Open("input")
+	file, _ := os.Open("example")
+	reader := bufio.NewReader(file)
+
+	instructions, _ := reader.ReadString('\n')
+	fmt.Println(instructions)
+	reader.ReadString('\n')
+
+	nodes, nameToNodeMap := readFileAndStructureInput(*reader)
+
 	fmt.Println(nodes)
 	fmt.Println(nameToNodeMap)
 }
