8d10f5593761504399ad6fa1dab6f90070e6f597
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-12-09 12:03:24 +0100

    Solve part2 in C#.

--- 2023/day09/part2/Program.cs
+++ 2023/day09/part2/Program.cs
@@ -18,7 +18,7 @@
         }
         Console.Write("\n");
     }
-    private static int GetNextNumberForList(List<int> nums)
+    private static int GetPrevNumberForList(List<int> nums)
     {
         Console.WriteLine("Skal processere denne listen: ");
         PrintIntList(nums);
@@ -38,7 +38,8 @@
             lastHistory = new List<int>(diffs);
             histories.Add(lastHistory);
         }
-        lastHistory.Add(0);
+        //lastHistory.Add(0);
+        lastHistory.Insert(0, 0);
         for (int i = histories.Count - 1; i > 0; i--)
         {
             List<int> history = histories[i];
@@ -48,13 +49,19 @@
             List<int> nextHistory = histories[i - 1];
             PrintIntList(nextHistory);
 
-            int toApp = history.Last() + nextHistory.Last();
+            //int toApp = history.First() + nextHistory.First();
+            int toApp = nextHistory.First() - history.First();
             Console.WriteLine("\t Legger til: " + toApp);
-            nextHistory.Add(toApp);
+            //nextHistory.Add(toApp);
+            //nextHistory.Insert(toApp, 0);
+            nextHistory.Insert(0, toApp);
+            PrintIntList(nextHistory);
 
         }
+        int res = histories[0].First();
+        Console.WriteLine("Resultatet er: " + res);
         Console.WriteLine("-----------------------------------------------");
-        return histories[0].Last();
+        return histories[0].First();
 
         /*
                 // sum last values of each history
@@ -74,52 +81,20 @@
     }
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("The current time is " + DateTime.Now);
-        Console.WriteLine("Args: " + string.Join(", ", args));
-
-
-        List<string> listOfLines = new();
-        List<List<int>> nums = new();
-
-        // open file for reading
+        int part2 = 0;
         using (var reader = new StreamReader(args[1]))
         {
-            // read the first line from the file
             string? line = reader.ReadLine();
-
-            // loop over the lines in the file
             while (line != null)
             {
-                // write the line to console window
-                Console.WriteLine(line);
-                listOfLines.Add(line);
-                // split line at space 
-                string[] split = line.Split(' ');
+                string[] split = line.Split(" ");
                 List<int> lineNums = new();
                 foreach (string s in split)
-                {
-                    // convert each number to an int
-                    int num = int.Parse(s);
-                    lineNums.Add(num);
-                }
-                nums.Add(lineNums);
-                // read the next line
+                    lineNums.Add(int.Parse(s));
+                part2 += GetPrevNumberForList(lineNums);
                 line = reader.ReadLine();
             }
         }
-        Console.WriteLine("Number of lines: " + listOfLines.Count);
-        Console.WriteLine("Number of numbers: " + nums.Count);
-        Console.WriteLine(nums);
-        Console.WriteLine(nums[0]);
-
-        List<int> processedVals = new();
-        foreach (List<int> line in nums)
-        {
-            int sum = GetNextNumberForList(line);
-            processedVals.Add(sum);
-        }
-        int res = processedVals.Sum();
-        Console.WriteLine("Part 2: " + res);
+        Console.WriteLine("Part 2: " + part2);
     }
 }
