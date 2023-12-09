51ab42b2d3ff93849dd9a127bb93c66a9ed807db
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-12-09 12:08:30 +0100

    Both parts in 1 file.

--- 2023/day09/both/Program.cs
+++ 2023/day09/both/Program.cs
@@ -10,6 +10,29 @@
         }
         return diffs;
     }
+    private static int GetPrevNumberForList(List<int> nums)
+    {
+        List<List<int>> histories = new();
+        List<int> diffs = new(nums);
+        List<int> lastHistory = new(nums);
+        histories.Add(lastHistory);
+        while (!diffs.All(x => x == 0))
+        {
+            diffs = GetDiffsForList(lastHistory);
+            lastHistory = new List<int>(diffs);
+            histories.Add(lastHistory);
+        }
+        lastHistory.Insert(0, 0);
+        for (int i = histories.Count - 1; i > 0; i--)
+        {
+            List<int> history = histories[i];
+            List<int> nextHistory = histories[i - 1];
+            int toApp = nextHistory.First() - history.First();
+            nextHistory.Insert(0, toApp);
+
+        }
+        return histories[0].First();
+    }
     private static int GetNextNumberForList(List<int> nums)
     {
         List<List<int>> histories = new();
@@ -33,6 +56,7 @@
     private static void Main(string[] args)
     {
         int part1 = 0;
+        int part2 = 0;
         using (var reader = new StreamReader(args[1]))
         {
             string? line = reader.ReadLine();
@@ -43,9 +67,11 @@
                 foreach (string s in split)
                     lineNums.Add(int.Parse(s));
                 part1 += GetNextNumberForList(lineNums);
+                part2 += GetPrevNumberForList(lineNums);
                 line = reader.ReadLine();
             }
         }
         Console.WriteLine("Part 1: " + part1);
+        Console.WriteLine("Part 2: " + part2);
     }
 }
