205570d478c5626a631bcac12dc844f612b728ab
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-12-09 10:56:00 +0100

    Read input.

--- 2023/day09/part1/Program.cs
+++ 2023/day09/part1/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine("The current time is " + DateTime.Now);
+Console.WriteLine("Args: " + string.Join(", ", args));
+
+
+// open file for reading
+using (var reader = new StreamReader(args[1]))
+{
+    // read the first line from the file
+    string line = reader.ReadLine();
+
+    // loop over the lines in the file
+    while (line != null)
+    {
+        // write the line to console window
+        Console.WriteLine(line);
+        // read the next line
+        line = reader.ReadLine();
+    }
+}
