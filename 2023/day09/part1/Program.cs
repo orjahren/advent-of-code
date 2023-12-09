234e7ef5d50b97f4fb52389fb4d00279cdf4da7a
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-12-09 11:01:06 +0100

    Program has a Main-method.

--- 2023/day09/part1/Program.cs
+++ 2023/day09/part1/Program.cs
@@ -1,23 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-
-Console.WriteLine("Hello, World!");
-Console.WriteLine("The current time is " + DateTime.Now);
-Console.WriteLine("Args: " + string.Join(", ", args));
-
-
-// open file for reading
-using (var reader = new StreamReader(args[1]))
+﻿internal class Program
 {
-    // read the first line from the file
-    string line = reader.ReadLine();
-
-    // loop over the lines in the file
-    while (line != null)
+    private static void Main(string[] args)
     {
-        // write the line to console window
-        Console.WriteLine(line);
-        // read the next line
-        line = reader.ReadLine();
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("The current time is " + DateTime.Now);
+        Console.WriteLine("Args: " + string.Join(", ", args));
+
+
+        List<string> listOfLines = new();
+
+        // open file for reading
+        using (var reader = new StreamReader(args[1]))
+        {
+            // read the first line from the file
+            string line = reader.ReadLine();
+
+            // loop over the lines in the file
+            while (line != null)
+            {
+                // write the line to console window
+                Console.WriteLine(line);
+                listOfLines.Add(line);
+                // read the next line
+                line = reader.ReadLine();
+            }
+        }
+        Console.WriteLine("Number of lines: " + listOfLines.Count);
     }
 }
