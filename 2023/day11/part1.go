d4728a9a77ff4d57404ce3e4ab9bfe0d6fa16921
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-12-11 12:18:40 +0100

    Find what rows/cols are empty.

--- 2023/day11/part1.go
+++ 2023/day11/part1.go
@@ -29,19 +29,42 @@ func main() {
 
 	points := make([]Point, 0)
 	y := 0
+	rowsOfOnlyDots := make([]int, 0)
+	colsOfOnlyDots := make([]int, 0)
+
 	for line != "" {
 		trimmedLine := strings.TrimSpace(line)
 		fmt.Println(trimmedLine)
 		line, _ = reader.ReadString('\n')
+		rowHasOnlyDots := true
 		for i, char := range trimmedLine {
 			if char == '#' {
 				p := Point{i, y}
 				fmt.Println(p)
 				points = append(points, p)
+				rowHasOnlyDots = false
 			}
 
 		}
+		if rowHasOnlyDots {
+			rowsOfOnlyDots = append(rowsOfOnlyDots, y)
+		}
 		y++
 	}
+
+	for i := 0; i < y; i++ {
+		colHasOnlyDots := true
+		for _, point := range points {
+			if point.x == i {
+				colHasOnlyDots = false
+			}
+		}
+		if colHasOnlyDots {
+			colsOfOnlyDots = append(colsOfOnlyDots, i)
+		}
+	}
+	// print what cols and rows have only dots
+	fmt.Println(rowsOfOnlyDots)
+	fmt.Println(colsOfOnlyDots)
 
 }
