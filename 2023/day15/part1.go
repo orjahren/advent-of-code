65754a49ec37e5c5d855318795725f209a20a96d
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-12-15 10:51:22 +0100

    Solve part1 in Go.

--- 2023/day15/part1.go
+++ 2023/day15/part1.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func processString(s string, ch chan int) {
+	current := 0
+	for _, c := range s {
+		current += int(c)
+		current *= 17
+		current %= 256
+	}
+	fmt.Println(s, " -> ", current)
+
+	ch <- current
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	//file, _ := os.Open("example")
+	file, _ := os.Open("input")
+	reader := bufio.NewReader(file)
+	ch := make(chan int)
+	nAwait := 0
+	line, _ := reader.ReadString('\n')
+	for line != "" {
+		fmt.Println(line)
+		spl := strings.Split(strings.Trim(line, "\n"), ",")
+		for _, s := range spl {
+			go processString(s, ch)
+			nAwait++
+		}
+		line, _ = reader.ReadString('\n')
+	}
+	part1 := 0
+	for nAwait > 0 {
+		part1 += <-ch
+		nAwait--
+	}
+	fmt.Println("Part 1: ", part1)
+
 }
