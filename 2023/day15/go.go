281dbc256eee2b6a1baebad358c62a2f059d9ff9
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-12-15 13:22:15 +0100

    More cleanup.

--- 2023/day15/go.go
+++ 2023/day15/go.go
@@ -34,47 +34,38 @@ func main() {
 	part1Ch := make(chan int)
 	line, _ := reader.ReadString('\n')
 	part2Map := make([][]*Node, 256)
-	nodeMap := make(map[string]Node)
+	nodeMap := make(map[string]*Node)
 	nAwaitPart1 := 0
 	for line != "" {
 		spl := strings.Split(strings.Trim(line, "\n"), ",")
 		for _, s := range spl {
-			go part1Hash(s, part1Ch)
-			nAwaitPart1++
 			if len(s) > 1 {
-				// BEGIN part 2
-				hasDash := strings.Contains(s, "-")
-				var spl []string
-				if hasDash {
-					spl = strings.Split(s, "-")
+				go part1Hash(s, part1Ch)
+				nAwaitPart1++
+				if strings.Contains(s, "-") {
+					spl := strings.Split(s, "-")
 					label := spl[0]
 					hh := getHash(label)
-					for i, n := range part2Map[hh] {
-						if n.label == label {
-							part2Map[hh] = append(part2Map[hh][:i], part2Map[hh][i+1:]...)
-							break
+					newSlice := make([]*Node, 0)
+					for _, n := range part2Map[hh] {
+						if n.label != label {
+							newSlice = append(newSlice, n)
 						}
 					}
+					part2Map[hh] = newSlice
+					delete(nodeMap, label)
 				} else {
-					spl = strings.Split(s, "=")
+					spl := strings.Split(s, "=")
 					label := spl[0]
 					val, _ := strconv.Atoi(spl[1])
-					hh := getHash(label)
-					node := Node{val, label, hh}
-					nodeMap[label] = node
-					madeChange := false
-					for i, n := range part2Map[hh] {
-						if n.label == label {
-							part2Map[hh][i].value = val
-							madeChange = true
-							break
-						}
-					}
-					if !madeChange {
-						part2Map[hh] = append(part2Map[hh], &node)
+					if other, nodeWithLabelExists := nodeMap[label]; nodeWithLabelExists {
+						other.value = val
+					} else {
+						node := Node{val, label, getHash(label)}
+						part2Map[node.hash] = append(part2Map[node.hash], &node)
+						nodeMap[label] = &node
 					}
 				}
-				// END part 2
 			}
 		}
 		line, _ = reader.ReadString('\n')
