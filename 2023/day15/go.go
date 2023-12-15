2bde8f466c025f43e793908a848341a06825ff8e
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-12-15 11:56:19 +0100

    Rename vars for part1.

--- 2023/day15/go.go
+++ 2023/day15/go.go
@@ -7,34 +7,34 @@ import (
 	"strings"
 )
 
-func processString(s string, ch chan int) {
+func part1Hash(s string, part1Ch chan int) {
 	current := 0
 	for _, c := range s {
 		current += int(c)
 		current *= 17
 		current %= 256
 	}
-	ch <- current
+	part1Ch <- current
 }
 
 func main() {
 	file, _ := os.Open("input")
 	reader := bufio.NewReader(file)
-	ch := make(chan int)
+	part1Ch := make(chan int)
 	nAwait := 0
 	line, _ := reader.ReadString('\n')
 	for line != "" {
 		spl := strings.Split(strings.Trim(line, "\n"), ",")
 		for _, s := range spl {
-			go processString(s, ch)
+			go part1Hash(s, part1Ch)
 			nAwait++
 		}
 		line, _ = reader.ReadString('\n')
 	}
-	part1 := 0
+	part1Res := 0
 	for nAwait > 0 {
-		part1 += <-ch
+		part1Res += <-part1Ch
 		nAwait--
 	}
-	fmt.Println("Part 1: ", part1)
+	fmt.Println("Part 1: ", part1Res)
 }
