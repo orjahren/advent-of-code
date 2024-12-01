7d60d5fc13918ab01bb21080af15b3732f77b743
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2024-12-01 13:50:34 +0100

    Day 1 part 1.

--- 2024/day01/cpp.cpp
+++ 2024/day01/cpp.cpp
@@ -1,6 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
-  int n;
-  cin >> n;
+int main()
+{
+  int res = 0, a, b;
+  vector<int> x, y;
+  while (cin >> a >> b)
+  {
+    x.push_back(a);
+    y.push_back(b);
+  }
+  sort(x.begin(), x.end());
+  sort(y.begin(), y.end());
+  for (int i = 0; i < x.size(); i++)
+    res += abs(x[i] - y[i]);
+  cout << res << endl;
 }
