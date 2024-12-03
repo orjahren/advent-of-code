f45f59c0088338c8174ff22135532bc6d3b4567f
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2024-12-03 11:16:32 +0100

    DRY.

--- 2024/day03/py.py
+++ 2024/day03/py.py
@@ -2,6 +2,8 @@ import re
 
 s = open(0).read()
 
+mul_ex = r"mul\(\d{1,3},\d{1,3}\)"
+
 
 def eval_hit(hit: str) -> int:
     l, r = hit[4:-1].split(",")
@@ -9,13 +11,12 @@ def eval_hit(hit: str) -> int:
 
 
 def part1():
-    ex = r"mul\(\d{1,3},\d{1,3}\)"
-    return sum(eval_hit(hit) for hit in re.findall(ex, s))
+    return sum(eval_hit(hit) for hit in re.findall(mul_ex, s))
 
 
 def part2() -> int:
     f = 1
-    ex = r"(do\(\))|(don't\(\))|(mul\(\d{1,3},\d{1,3}\))"
+    ex = r"(do\(\))|(don't\(\))|(" + mul_ex + ")"
     return sum(((f := do and (not dont) or (mul and f)) and not do and eval_hit(mul) or 0)for do, dont, mul in re.findall(ex, s))
 
 
