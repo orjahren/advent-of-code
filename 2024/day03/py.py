3c6d1b270716dbf0f16c94beb3bee20ec5cee7f9
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2024-12-03 11:18:02 +0100

    Må lese til eksamen nå.

--- 2024/day03/py.py
+++ 2024/day03/py.py
@@ -10,15 +10,11 @@ def eval_hit(hit: str) -> int:
     return int(l) * int(r)
 
 
-def part1():
-    return sum(eval_hit(hit) for hit in re.findall(mul_ex, s))
-
-
 def part2() -> int:
     f = 1
     ex = r"(do\(\))|(don't\(\))|(" + mul_ex + ")"
     return sum(((f := do and (not dont) or (mul and f)) and not do and eval_hit(mul) or 0)for do, dont, mul in re.findall(ex, s))
 
 
-print("Part 1:", part1())
+print("Part 1:", sum(eval_hit(hit) for hit in re.findall(mul_ex, s)))
 print("Part 2:", part2())
