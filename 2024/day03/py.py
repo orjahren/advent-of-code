8b4d55bcd7b4abfef1da9b23b751d9709e651c94
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2024-12-03 10:39:41 +0100

    Solve part 2.

--- 2024/day03/py.py
+++ 2024/day03/py.py
@@ -13,10 +13,38 @@ def eval_hit(hit: str) -> int:
     return r * l
 
 
-ex = r"mul\(\d{1,3},\d{1,3}\)"
-hits = re.findall(ex, s)
+def part1():
+    ex = r"mul\(\d{1,3},\d{1,3}\)"
+    hits = re.findall(ex, s)
+
+    return sum(eval_hit(hit) for hit in hits)
 
 
-res = sum(eval_hit(hit) for hit in hits)
+def part2():
+    # ex = r"(do\(\))|(don't\(\))|(mul\(\d{1,3},\d{1,3}\))"
+    ex = r"(do\(\))|(don't\(\))|(do_not\(\))|(mul\(\d{1,3},\d{1,3}\))"
+    hits = re.findall(ex, s)
 
-print(res)
+    # print(hits)
+
+    res = 0
+    should_mul = True
+
+    for hit in hits:
+        print(hit)
+        if hit[1] or hit[2]:
+            should_mul = False
+            print("SKRUR AV MUL")
+        elif hit[0]:
+            should_mul = True
+            print("SKRUR på MUL")
+            continue
+
+        if should_mul:
+            res += eval_hit(hit[3])
+
+    return res
+
+
+# print("Part 1:", part1())
+print("Part 2:", part2())
