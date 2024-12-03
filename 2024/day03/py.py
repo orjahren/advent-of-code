8e240d94b6f52783a3521b6aefae80cb159cdf64
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2024-12-03 10:14:18 +0100

    Solve part1.

--- 2024/day03/py.py
+++ 2024/day03/py.py
@@ -1,2 +1,22 @@
-s = input()
+import re
+
+s = open(0).read()
 print(s)
+
+
+def eval_hit(hit: str) -> int:
+    print("HIT:", hit)
+    l, r = hit.split(",")
+    l = int(l[4:])
+    r = int(r[:-1])
+    print(l, r)
+    return r * l
+
+
+ex = r"mul\(\d{1,3},\d{1,3}\)"
+hits = re.findall(ex, s)
+
+
+res = sum(eval_hit(hit) for hit in hits)
+
+print(res)
