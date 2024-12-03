9da668a662b2b9594c2fa276923d1f15725e14d9
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2024-12-03 10:41:10 +0100

    Cleanup.

--- 2024/day03/py.py
+++ 2024/day03/py.py
@@ -1,16 +1,11 @@
 import re
 
 s = open(0).read()
-print(s)
 
 
 def eval_hit(hit: str) -> int:
-    print("HIT:", hit)
-    l, r = hit.split(",")
-    l = int(l[4:])
-    r = int(r[:-1])
-    print(l, r)
-    return r * l
+    l, r = hit[4:-1].split(",")
+    return int(l) * int(r)
 
 
 def part1():
@@ -21,23 +16,17 @@ def part1():
 
 
 def part2():
-    # ex = r"(do\(\))|(don't\(\))|(mul\(\d{1,3},\d{1,3}\))"
     ex = r"(do\(\))|(don't\(\))|(do_not\(\))|(mul\(\d{1,3},\d{1,3}\))"
     hits = re.findall(ex, s)
-
-    # print(hits)
 
     res = 0
     should_mul = True
 
     for hit in hits:
-        print(hit)
         if hit[1] or hit[2]:
             should_mul = False
-            print("SKRUR AV MUL")
         elif hit[0]:
             should_mul = True
-            print("SKRUR på MUL")
             continue
 
         if should_mul:
@@ -46,5 +35,5 @@ def part2():
     return res
 
 
-# print("Part 1:", part1())
+print("Part 1:", part1())
 print("Part 2:", part2())
