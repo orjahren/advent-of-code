1c12514d1659ea9228d679cf343149bb750519d4
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2024-12-04 19:44:22 +0100

    Simplify part 1.

--- 2024/day04/py.py
+++ 2024/day04/py.py
@@ -1,16 +1,4 @@
-from typing import Dict, List, Set, Tuple
-
-type Path = List[Tuple[int, int]]
-
-
-def get_neighbor(x: int, y: int, direction: Tuple[int, int], grid) -> Tuple[int, int]:
-    def cand_is_in_bounds(x: int, y: int) -> bool:
-        return x >= 0 and x < len(grid) and y >= 0 and y < len(grid[0])
-    xp, yp = direction
-    cand = x + xp, y + yp
-    if cand_is_in_bounds(*cand):
-        return cand
-    return None
+from typing import List, Set, Tuple
 
 
 def get_letter_pos(grid, letter):
@@ -18,39 +6,32 @@ def get_letter_pos(grid, letter):
 
 
 def part1(grid):
-    TARGET_STR = "XMAS"
 
-    def recurse(
-        tup: Tuple[int, int],
-        path: Path,
-        visited_set: Set[Tuple[int, int]],
-        target: List[Path],
-        direction: Tuple[int, int],
-    ) -> None:
-        is_finished = len(path) == len(TARGET_STR)
-        nei = get_neighbor(*tup, direction, grid)
-        if is_finished:
-            target.append(path)
-        if not nei or is_finished:
-            return
-        x, y = nei
-        ch = grid[x][y]
-        visited_set.add(nei)
-        exp_ch = TARGET_STR[len(path)]
-        if ch == exp_ch:
-            path.append(nei)
-            recurse(nei, list(path), set(visited_set), target, direction)
+    def cand_is_in_bounds(x: int, y: int) -> bool:
+        return x >= 0 and x < len(grid) and y >= 0 and y < len(grid[0])
 
-    def do_search(start_pos: Tuple[int, int]) -> List[List[Tuple[int, int]]]:
-        res = []
-        # Up, Down, Left, Right, and Diagonals
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1),
-                      (-1, -1), (-1, 1), (1, -1), (1, 1)]
-        for direction in directions:
-            recurse(start_pos, [start_pos], set([start_pos]), res, direction)
-        return res
-    x_positions = get_letter_pos(grid, "X")
-    return len([x for xs in filter(lambda x: len(x) > 0, [do_search((x, y)) for x, y in x_positions]) for x in xs])
+    res = 0
+    for tup in get_letter_pos(grid, "X"):
+        for dr in (-1, 0, 1):
+            for dc in (-1, 0, 1):
+                if dr == dc == 0:
+                    continue
+                print(dr, dc)
+                r, c = tup
+
+                end_tup = r + 3 * dr, c + 3 * dc
+                if not cand_is_in_bounds(*end_tup):
+                    continue
+
+                s1 = grid[r + dr][c + dc]
+                s2 = grid[r + 2 * dr][c + 2 * dc]
+                s3 = grid[r + 3 * dr][c + 3 * dc]
+
+                print(s1, s2, s3)
+
+                if s1 == "M" and s2 == "A" and s3 == "S":
+                    res += 1
+    return res
 
 
 def part2(grid):
@@ -109,8 +90,8 @@ def part2(grid):
 
 def main():
     grid = [list(y) for x in open(0).readlines() if (y := x.strip())]
-    # print("Part1:", part1(grid))
-    print("Part2:", part2(grid))
+    print("Part1:", part1(grid))
+    # print("Part2:", part2(grid))
 
 
 main()
