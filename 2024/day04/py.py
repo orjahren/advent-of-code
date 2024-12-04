60187c6280e5a3a08fb6beab5826b021410c1711
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2024-12-04 16:17:52 +0100

    Modularization.

--- 2024/day04/py.py
+++ 2024/day04/py.py
@@ -3,26 +3,21 @@ from typing import Dict, List, Set, Tuple
 Path = List[Tuple[int, int]]
 TARGET_STR = "XMAS"
 
-global_grid = [list(y) for x in open(0).readlines() if (y := x.strip())]
 
-x_positions = [(y, x) for y, l in enumerate(global_grid)
-               for x, sl in enumerate(l) if sl == "X"]
-
-
-def cand_is_in_bounds(x: int, y: int) -> bool:
-    return x >= 0 and x < len(global_grid) and y >= 0 and y < len(global_grid[0])
+def cand_is_in_bounds(x: int, y: int, grid) -> bool:
+    return x >= 0 and x < len(grid) and y >= 0 and y < len(grid[0])
 
 
-def get_neighbor(x: int, y: int, direction: Tuple[int, int]) -> Tuple[int, int]:
+def get_neighbor(x: int, y: int, direction: Tuple[int, int], grid) -> Tuple[int, int]:
     xp, yp = direction
     cand = x + xp, y + yp
-    if cand_is_in_bounds(*cand):
+    if cand_is_in_bounds(*cand, grid):
         return cand
     return None
 
 
-def path_to_str(path: List[Tuple[int, int]]) -> str:
-    return "".join([global_grid[x][y] for x, y in path])
+def path_to_str(path: List[Tuple[int, int]], grid) -> str:
+    return "".join([grid[x][y] for x, y in path])
 
 
 def recurse(
@@ -31,40 +26,43 @@ def recurse(
     visited_set: Set[Tuple[int, int]],
     target: List[Path],
     direction: Tuple[int, int],
+    grid
 ) -> None:
     is_finished = len(path) == len(TARGET_STR)
-    nei = get_neighbor(*tup, direction)
+    nei = get_neighbor(*tup, direction, grid)
     if (nei in visited_set) or is_finished:
-        if is_finished and path_to_str(path) == TARGET_STR:
+        if is_finished and path_to_str(path, grid) == TARGET_STR:
             target.append(path)
         return
     if not nei:
         return
     x, y = nei
-    ch = global_grid[x][y]
+    ch = grid[x][y]
     visited_set.add(nei)
     exp_ch = TARGET_STR[len(path)]
     if ch == exp_ch:
         path.append(nei)
-        recurse(nei, list(path), set(visited_set), target, direction)
+        recurse(nei, list(path), set(visited_set), target, direction, grid)
 
 
-def do_search(start_pos: Tuple[int, int]) -> List[List[Tuple[int, int]]]:
+def do_search(start_pos: Tuple[int, int], grid) -> List[List[Tuple[int, int]]]:
     res = []
     # Up, Down, Left, Right, and Diagonals
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1),
                   (-1, -1), (-1, 1), (1, -1), (1, 1)]
     for direction in directions:
-        recurse(start_pos, [start_pos], set([start_pos]), res, direction)
+        recurse(start_pos, [start_pos], set([start_pos]), res, direction, grid)
     return res
 
 
-def main():
+def part1(grid):
+    x_positions = [(y, x) for y, l in enumerate(grid)
+                   for x, sl in enumerate(l) if sl == "X"]
 
     global_paths = []
     rd: Dict[Tuple[int, int], Path] = {}
     for x, y in x_positions:
-        res = do_search((x, y))
+        res = do_search((x, y), grid)
         if len(res) > 0:
             rd[x, y] = res
             for p in res:
@@ -74,7 +72,12 @@ def main():
     flat_paths = [tup for xs in global_paths for tup in xs]
     flat_paths = list(set(flat_paths))
 
-    print("Res:", len(global_paths))
+    print("Part1:", len(global_paths))
+
+
+def main():
+    grid = [list(y) for x in open(0).readlines() if (y := x.strip())]
+    part1(grid)
 
 
 main()
