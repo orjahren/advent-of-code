ed1520cfd5b2d2c26cb41a009ce02ca4d6bc30e2
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2024-12-07 14:18:32 +0100

    Solve pt2 (6s runtime).

--- 2024/day07/go.go
+++ 2024/day07/go.go
@@ -9,20 +9,21 @@ import (
 )
 
 type Equation struct {
-	value   int
-	numbers []int
+	value   int64
+	numbers []int64
 }
 
 func parseLine(line string) Equation {
 	spl := strings.Split(line, " ")
 	fmt.Println(spl)
-	nums := make([]int, len(spl)-1)
+	nums := make([]int64, len(spl)-1)
 	for numIdx := 1; numIdx < len(spl); numIdx++ {
-		nums[numIdx-1], _ = strconv.Atoi(spl[numIdx])
+		x, _ := strconv.Atoi(spl[numIdx])
+		nums[numIdx-1] = int64(x)
 	}
 	spl[0] = strings.Replace(spl[0], ":", "", -1)
 	val, _ := strconv.Atoi(spl[0])
-	return Equation{val, nums}
+	return Equation{int64(val), nums}
 }
 
 func readInput(scanner *bufio.Scanner) []Equation {
@@ -41,28 +42,82 @@ func readInput(scanner *bufio.Scanner) []Equation {
 	return lines
 }
 
-func solveEquation(equation Equation, curr, numIdx int) int {
+func solveEquationP1(equation Equation, curr, numIdx int) int {
 	operators := []string{"+", "*"}
-	if numIdx == len(equation.numbers) {
-		if curr == equation.value {
-			return 1
+	if int(numIdx) == len(equation.numbers) {
+		if curr == int(equation.value) {
+			return curr
 		}
-		return -1
+		return 0
 	}
 	for _, op := range operators {
 		if op == "+" {
-			x := solveEquation(equation, curr+equation.numbers[numIdx], numIdx+1)
+			x := solveEquationP1(equation, curr+int(equation.numbers[numIdx]), numIdx+1)
 			if x != -1 {
-				return equation.value
+				return int(equation.value)
 			}
 		} else {
-			x := solveEquation(equation, curr*equation.numbers[numIdx], numIdx+1)
+			x := solveEquationP1(equation, curr*int(equation.numbers[numIdx]), numIdx+1)
 			if x != -1 {
-				return equation.value
+				return int(equation.value)
 			}
 		}
 	}
-	return -1
+	return 0
+}
+
+func raise(base, exp int) int {
+	result := 1
+	for i := 0; i < exp; i++ {
+		result *= base
+	}
+	return result
+}
+
+func getBaseRep(num, base int) string {
+	if num == 0 {
+		return "0"
+	}
+	res := ""
+	for num > 0 {
+		res = strconv.FormatInt(int64(num%base), 10) + res
+		num /= base
+	}
+	return res
+}
+
+const testTarget = 7290
+
+func solveEquationP2(equation Equation) int {
+	if equation.value == testTarget {
+		fmt.Println("[top] Origial equation: ", equation)
+	}
+	slots := len(equation.numbers) - 1
+	for p := 0; p < raise(3, slots); p++ {
+		res := equation.numbers[0]
+		s := getBaseRep(p, 3)
+		l := len(s)
+
+		for b := 0; b < slots; b++ {
+			bv := 0
+			if l-b-1 >= 0 {
+				bv = int(s[l-b-1] - '0')
+			}
+			if bv == 0 {
+				res += equation.numbers[b+1]
+			} else if bv == 1 {
+				res *= equation.numbers[b+1]
+			} else if bv == 2 {
+				res, _ = strconv.ParseInt(fmt.Sprintf("%d%d", res, equation.numbers[b+1]), 10, 64)
+			}
+		}
+
+		if res == equation.value {
+			return int(equation.value)
+		}
+	}
+
+	return 0
 }
 
 func main() {
@@ -72,14 +127,18 @@ func main() {
 
 	p1 := 0
 	for _, eq := range equations {
-		fmt.Println(eq)
-		x := solveEquation(eq, 0, 0)
-		if x != -1 {
-			p1 += x
-
-		}
+		p1 += solveEquationP1(eq, 0, 0)
 	}
 
 	fmt.Println("Part 1:", p1)
+
+	p2 := 0
+	for _, eq := range equations {
+		x := solveEquationP2(eq)
+		if x != 0 {
+			p2 += int(x)
+		}
+	}
+	fmt.Println("Part 2:", p2)
 
 }
