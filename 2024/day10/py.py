bbf59f5657648e44c4ffe31a0d76cae61d111fd7
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2024-12-10 14:33:32 +0100

    Troll.

--- 2024/day10/py.py
+++ 2024/day10/py.py
@@ -7,24 +7,17 @@ def cand_is_in_bounds(x: int, y: int, grid) -> bool:
 
 
 def get_neighbors(r, c):
-    yield from ((r + dr, c + dc) for dr, dc in ((-1, 0), (1, 0), (0, -1), (0, 1))if cand_is_in_bounds(r + dr, c + dc, grid))
+    yield from ((r + dr, c + dc) for dr, dc in ((-1, 0), (1, 0), (0, -1), (0, 1))if cand_is_in_bounds(r + dr, c + dc, grid) and diff((r, c), (r+dr,c+dc)) == 1)
 
 
-def neighbor_is_ok(_from, to):
+def diff(_from, to):
     fr, fc = _from
     tr, tc = to
-    fx = int(grid[fr][fc])
-    tx = int(grid[tr][tc])
-    diff = tx - fx
-    return diff == 1
-
+    return int(grid[tr][tc]) - int(grid[fr][fc])
 
 def is_target(tup):
     r, c = tup
     return grid[r][c] == "9"
-
-
-grid = [list(y) for x in open(0).readlines() if (y := x.strip())]
 
 
 def get_trailhood_score(trailhead):
@@ -33,10 +26,11 @@ def get_trailhood_score(trailhead):
         curr = q.pop()
         if is_target(curr):
             d[curr] = d.get(curr, 0) + 1
-        q.extend((neighbor for neighbor in get_neighbors(*curr) if neighbor_is_ok(curr, neighbor)))
+        q.extend(neighbor for neighbor in get_neighbors(*curr))
     return len(d.keys()), sum(d.values())
 
 
-part1, part2 = map(sum, zip(*[get_trailhood_score(trailhead) for trailhead in get_letter_pos("0")]))
-print("Part1:", part1)
-print("Part2:", part2)
+grid = [list(y) for x in open(0).readlines() if (y := x.strip())]
+p1, p2 = map(sum, zip(*[get_trailhood_score(trailhead)for trailhead in get_letter_pos("0")]))
+print("Part1:", p1)
+print("Part2:", p2)
