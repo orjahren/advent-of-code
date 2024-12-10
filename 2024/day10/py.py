2d21260b209198fe5d8f334521ef9a72106e12e4
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2024-12-10 14:06:06 +0100

    Cleanup.

--- 2024/day10/py.py
+++ 2024/day10/py.py
@@ -14,13 +14,8 @@ def neighbor_is_ok(_from, to):
     fr, fc = _from
     tr, tc = to
     fx = int(grid[fr][fc])
-    vx = grid[tr][tc]
-    if vx == ".":
-        return 0
     tx = int(grid[tr][tc])
-    diff = fx - tx
     diff = tx - fx
-    print("Diff mellom", _from, fx, "og", to, tx, "er", diff)
     return diff == 1
 
 
@@ -30,50 +25,27 @@ def is_target(tup):
 
 
 grid = [list(y) for x in open(0).readlines() if (y := x.strip())]
-trailhead_ress = []
 
 
 def get_trailhood_score(trailhead):
-    r, c = trailhead
-    res = 0
-    print("0:", r, c)
-    q = [(r, c)]
-    # visited = set(q[0])
-    rs = {}
+    q = [trailhead]
+    p1, p2 = 0, {}
     while len(q):
-        print(q)
         curr = q.pop()
         if is_target(curr):
-            print("\t**** FOUND MATCH")
-            rs[curr] = rs.get(curr, 0) + 1
-
-            continue
+            if not curr in p2:
+                p1 += 1
+            p2[curr] = p2.get(curr, 0) + 1
         for neighbor in get_neighbors(*curr):
-            # for neighbor in graph[s]:
-            print(neighbor)
             if neighbor_is_ok(curr, neighbor):
-                print("OK")
-                # if neighbor not in visited:
                 q.append(neighbor)
-                # visited.add(neighbor)
-            else:
-                # print("Not OK")
-                pass
-    """
-    l = [grid[r + dr][c + dc]
-        for dr, dc in ((-1, -1), (-1, 1), (1, 1), (1, -1))if cand_is_in_bounds(r + dr, c + dc, grid)]
-    """
-    trailhead_ress.append(res)
-    # print(l)
-    return rs
+    return p1, sum(p2.values())
 
 
-trailheads = list(get_letter_pos("0"))
-print(trailheads)
-part1 = 0
-for trailhead in trailheads:
-    print(trailhead)
-    ts = get_trailhood_score(trailhead)
-    print(ts)
-    part1 += sum(ts.values())
+part1, part2 = 0, 0
+for trailhead in get_letter_pos("0"):
+    p1, p2 = get_trailhood_score(trailhead)
+    part1 += p1
+    part2 += p2
 print("Part1:", part1)
+print("Part2:", part2)
