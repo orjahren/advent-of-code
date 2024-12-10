01f515725d573939f73604b89f0e7727cba252b2
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2024-12-10 14:48:59 +0100

    Golf.

--- 2024/day10/py.py
+++ 2024/day10/py.py
@@ -1,26 +1,14 @@
-def get_letter_pos(letter):
-    yield from ((y, x) for y, l in enumerate(grid) for x, sl in enumerate(l) if sl == letter)
-
-
-def cand_is_in_bounds(x: int, y: int, grid) -> bool:
-    return x >= 0 and x < len(grid) and y >= 0 and y < len(grid[0])
-
-
-def get_neighbors(r, c):
-    yield from ((nr, nc) for dr, dc in ((-1, 0), (1, 0), (0, -1), (0, 1))if cand_is_in_bounds((nr := r + dr), (nc := c + dc), grid) and int(grid[nr][nc]) - int(grid[r][c]) == 1)
-
-
 def solve(trailhead):
+    cand_is_in_bounds = lambda x,  y, grid: x >= 0 and x < len(grid) and y >= 0 and y < len(grid[0])
     q, d = [trailhead], {}
     while len(q):
         r, c = q.pop()
         if grid[r][c] == "9":
             d[r, c] = d.get((r, c), 0) + 1
-        q += get_neighbors(r, c)
+        q += ((nr, nc) for dr, dc in ((-1, 0), (1, 0), (0, -1), (0, 1))if cand_is_in_bounds((nr := r + dr), (nc := c + dc), grid) and int(grid[nr][nc]) - int(grid[r][c]) == 1)
     return len(d.keys()), sum(d.values())
 
-
 grid = [list(y) for x in open(0).readlines() if (y := x.strip())]
-p1, p2 = map(sum, zip(*[solve(trailhead)for trailhead in get_letter_pos("0")]))
+p1, p2 = map(sum, zip(*[solve(trailhead)for trailhead in ((y, x)for y, l in enumerate(grid) for x, sl in enumerate(l) if sl == "0")]))
 print("Part1:", p1)
 print("Part2:", p2)
