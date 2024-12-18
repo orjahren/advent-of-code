1ba35905d8a8893d13aef10d9a7b0bc055f5c55f
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2024-12-18 13:07:08 +0100

    Part 2.

--- 2024/day18/cpp.cpp
+++ 2024/day18/cpp.cpp
@@ -30,38 +30,53 @@ struct triplet
 {
   int x, y, distance;
 };
-int getDistanceBfs(vector<vector<char>> &grid, int tx, int ty)
+int getDistanceBfs(vector<vector<char>> &grid, int trow, int tcol)
 {
   queue<triplet> q;
   q.push({0, 0, 0});
   auto visited = vector<vector<bool>>(grid.size(), vector<bool>(grid.size(), false));
   while (!q.empty())
   {
-    auto [x, y, distance] = q.front();
+    auto [col, row, distance] = q.front();
     q.pop();
-    if (x < 0 || x >= grid.size() || y < 0 || y >= grid.size())
+    if (col < 0 || col >= grid.size() || row < 0 || row >= grid.size())
     {
       continue;
     }
-    if (grid[y][x] == '#' || visited[y][x])
+    if (grid[col][row] == '#' || visited[col][row])
     {
       continue;
     }
-    visited[y][x] = true;
-    if (x == tx && y == ty)
+    visited[col][row] = true;
+    if (col == trow && row == tcol)
     {
       return distance;
     }
-    q.push({x + 1, y, distance + 1});
-    q.push({x - 1, y, distance + 1});
-    q.push({x, y + 1, distance + 1});
-    q.push({x, y - 1, distance + 1});
+    q.push({col + 1, row, distance + 1});
+    q.push({col - 1, row, distance + 1});
+    q.push({col, row + 1, distance + 1});
+    q.push({col, row - 1, distance + 1});
   }
   return -1;
 }
 
+vector<vector<char>> taintGrid(vector<vector<char>> grid, vector<pair<int, int>> &fallenBytes, int fallCount)
+{
+  auto newGrid = grid;
+  for (auto [col, row] : fallenBytes)
+  {
+    newGrid[col][row] = '#';
+    if (fallCount-- <= 0)
+    {
+      break;
+    }
+  }
+  return newGrid;
+}
+
 int main()
 {
+  // test values
   int size = 6;
   int nBytes = 12;
 
@@ -69,25 +84,34 @@ int main()
   size = 70;
   nBytes = 1024;
 
-  auto grid = getGrid(size);
-  printGrid(grid);
+  auto masterGrid = getGrid(size);
+  printGrid(masterGrid);
 
   char dn;
-  int x, y;
-  int fallen = 0;
-
-  while (cin >> x >> dn >> y)
+  int col, row;
+  vector<pair<int, int>> fallenBytes;
+  while (cin >> col >> dn >> row)
   {
-    if (fallen++ >= nBytes)
-    {
-      cout << "All bytes have fallen" << endl;
-      break;
-    }
-    cout << x << ", " << y << endl;
-    grid[y][x] = '#';
+    cout << col << ", " << row << endl;
+    fallenBytes.push_back({col, row});
   }
 
-  printGrid(grid);
-  auto bfs = getDistanceBfs(grid, size, size);
-  cout << "Distance: " << bfs << endl;
+  auto part1Grid = taintGrid(masterGrid, fallenBytes, nBytes - 1);
+  printGrid(part1Grid);
+  auto part1 = getDistanceBfs(part1Grid, size, size);
+  cout << "Part 1: " << part1 << endl;
+
+  /// part 2
+  for (int i = 0; i < fallenBytes.size(); i++)
+  {
+    cout << "i: " << i << ", " << "x: " << fallenBytes[i].first << " y: " << fallenBytes[i].second << endl;
+    auto part2Grid = taintGrid(masterGrid, fallenBytes, i);
+    printGrid(part2Grid);
+    auto part2 = getDistanceBfs(part2Grid, size, size);
+    if (part2 == -1)
+    {
+      cout << "Part 2: " << fallenBytes[i].first << "," << fallenBytes[i].second << endl;
+      return 0;
+    }
+  }
 }
