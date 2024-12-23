cd9cdbe292a01306b5feeae2508d28937dbf9148
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2024-12-23 10:35:03 +0100

    Cleanup.

--- 2024/day23/py.py
+++ 2024/day23/py.py
@@ -1,7 +1,27 @@
 # adjecency list is of the format [node node], where node is a string and there is an undirected edge between the two nodes
+def p1(graph):
+    used_sets = set()
+    results = []
+    for node, edges in graph.items():
+        for x in edges:
+            for y in graph[x]:
+                if y in edges:
+                    s = "".join(sorted([node, x, y]))
+                    if "t" in [v[0] for v in [node, x, y]]:
+                        if s not in used_sets:
+                            results.append((sorted([node, x, y])))
+                        used_sets.add(s)
+    return len(results)
 
 
-def p1(adjecency_list):
+def p2(graph):
+    pass
+
+
+def main():
+    adjecency_list = [x.split("-") for x in open(0).read().splitlines() if x]
+    print(adjecency_list)
+
     graph = {}
     for a, b in adjecency_list:
         if a not in graph:
@@ -12,37 +32,11 @@ def p1(adjecency_list):
         graph[b].append(a)
     print(graph)
 
-    res = 0
-    used_sets = set()
-    results = []
-    for node, edges in graph.items():
-        print(node, edges)
-        for x in edges:
-            for y in graph[x]:
-                if y in edges:
-                    s = "".join(sorted([node, x, y]))
-                    print("Vurderer", node, x, y, s)
-                    if "t" in [v[0] for v in [node, x, y]]:
-                        if s not in used_sets:
-                            print("found", node, x, y)
-                            res += 1
-                            results.append((sorted([node, x, y])))
-                        used_sets.add(s)
+    part1 = p1(graph)
+    print("Part 1:", part1)
 
-    print(used_sets)
-    print(results)
-    for x in sorted(results):
-        print(x)
-    return res
-
-
-def main():
-    adjecency_list = [x.split("-") for x in open(0).read().splitlines() if x]
-    print(adjecency_list)
-
-    print("Part 1:", p1(adjecency_list))
-    # 2490 too high
-    # 2298 too high
+    part2 = p2(graph)
+    print("Part 2:", part2)
 
 
 if __name__ == "__main__":
