878fa79cbbaf29c50440cc16f7e800549f34f419
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2024-12-23 14:52:00 +0100

    Solve pt2.

--- 2024/day23/py.py
+++ 2024/day23/py.py
@@ -1,4 +1,3 @@
-# adjecency list is of the format [node node], where node is a string and there is an undirected edge between the two nodes
 def p1(graph):
     used_sets = set()
     results = []
@@ -7,15 +6,28 @@ def p1(graph):
             for y in graph[x]:
                 if y in edges:
                     s = "".join(sorted([node, x, y]))
-                    if "t" in [v[0] for v in [node, x, y]]:
-                        if s not in used_sets:
-                            results.append((sorted([node, x, y])))
+                    if "t" in [v[0] for v in [node, x, y]] and s not in used_sets:
+                        results.append((sorted([node, x, y])))
                         used_sets.add(s)
     return len(results)
 
 
 def p2(graph):
-    pass
+    res = set()
+
+    def rec(node, required):
+        if (k := tuple(sorted(required))) in res:
+            return
+        res.add(k)
+        for x in graph[node]:
+            if x in required or not all(y in graph[x] for y in required):
+                continue
+            rec(x, required | {x})
+    for node in graph:
+        rec(node,  {node})
+
+    ret = max(res, key=len)
+    return ",".join(sorted(ret, key=lambda x: x))
 
 
 def main():
