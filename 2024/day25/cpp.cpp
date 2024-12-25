952048ff0e1e8b2a903f09fa33260ed8a44abcec
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2024-12-25 10:53:20 +0100

    DRY

--- 2024/day25/cpp.cpp
+++ 2024/day25/cpp.cpp
@@ -1,30 +1,21 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+void addAndExhaust(vector<vector<string>> &target, string &line)
+{
+  target.push_back(vector<string>());
+  do
+  {
+    target.back().push_back(line);
+  } while (getline(cin, line) && !line.empty());
+}
 pair<vector<vector<string>>, vector<vector<string>>> readInput()
 {
   vector<vector<string>> locks;
   vector<vector<string>> keys;
   string line;
   while (getline(cin, line))
-  {
-    if (line[0] == '#')
-    {
-      locks.push_back(vector<string>());
-      do
-      {
-        locks.back().push_back(line);
-      } while (getline(cin, line) && !line.empty());
-    }
-    else
-    {
-      keys.push_back(vector<string>());
-      do
-      {
-        keys.back().push_back(line);
-      } while (getline(cin, line) && !line.empty());
-    }
-  }
+    addAndExhaust(line[0] == '#' ? locks : keys, line);
   return {locks, keys};
 }
 
@@ -34,39 +25,24 @@ int *objToNumbers(vector<string> obj, string blockChar)
   int width = obj[0].size();
   int *columnHeights = new int[width];
   for (int i = 0; i < width; i++)
-  {
     if (blockChar == ".")
       columnHeights[i] = height;
     else
       columnHeights[i] = -1;
-  }
   for (int i = 0; i < height; i++)
-  {
     for (int j = 0; j < width; j++)
-    {
       if (obj[i][j] == blockChar[0] && blockChar == "#")
-      {
         columnHeights[j]++;
-      }
       else if (obj[i][j] == blockChar[0] && blockChar == ".")
-      {
         columnHeights[j] = height - i - 2;
-      }
-    }
-  }
-
   return columnHeights;
 }
 
 bool lockAndKeyOverlap(int *lock, int *key)
 {
   for (int i = 0; i < 5; i++)
-  {
     if ((lock[i] + key[i]) > 5)
-    {
       return true;
-    }
-  }
   return false;
 }
 
