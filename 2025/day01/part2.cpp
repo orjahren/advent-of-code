2a502c6ad449434d1e800f2e9c9bd9d22902318a
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2025-12-01 15:23:43 +0100

    Kortere.

--- 2025/day01/part2.cpp
+++ 2025/day01/part2.cpp
@@ -2,10 +2,6 @@
 
 using namespace std;
 
-int getIntFromStr(string &s)
-{
-    return stoi(s.substr(1));
-}
 int main()
 {
     int curr = 50;
@@ -13,26 +9,16 @@ int main()
     string inp;
     while (cin >> inp)
     {
-        cout << inp << endl;
-        int dist = getIntFromStr(inp);
-        cout << dist << endl;
-        for (int i = 0; i < dist; i++)
+        for (int i = 0; i < stoi(inp.substr(1)); i++)
         {
             if (curr == 0)
                 p2++;
-
             if (inp[0] == 'L')
-            {
                 curr--;
-            }
             else
-            {
                 curr++;
-            }
             curr %= 100;
         }
     }
     cout << "Part 2: " << p2 << endl;
-
-    return 0;
 }
