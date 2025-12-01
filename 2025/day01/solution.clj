f4d42b66c075a493698f68427312088d50185f5a
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2025-12-01 10:15:47 +0100

    Parse input.

--- 2025/day01/solution.clj
+++ 2025/day01/solution.clj
@@ -10,10 +10,23 @@
     ;(println content)
     content))
 
+
+;(defn rotate [direction old-state new-state]
+(defn rotate [direction]
+  (say (str "Direction: " direction))
+  ;(say (get direction 0))
+  (let [distance (subs direction 1)]
+    ;(say distance)
+    (if (identical? (get direction 0) \L) (say (str "Left med dist: " distance)) (say (str "Høgern med dist " distance))))
+  direction)
+
 (defn solve [file-name]
 ;(def input (read-file 'test-input))
   (let [lines (clojure.string/split-lines (read-file file-name))]
-    (say lines)))
+    ;(say lines)))
+    (say "Will map lines")
+    (let [curr 50])
+    (let [new-state (map rotate lines)] (say new-state))))
 
 
 (solve "example")
