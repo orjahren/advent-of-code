6329d6d67b169da93c219d7303b929d8a726c65b
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2025-12-02 09:06:24 +0100

    Parse input.

--- 2025/day02/Solution.java
+++ 2025/day02/Solution.java
@@ -2,9 +2,49 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+class Pair {
+    int left, right;
+
+    @Override
+    public String toString() {
+        return "{" + left + ", " + right + "}";
+    }
+}
 
 class Solution {
-    final private static String getLine(final String fileName) throws FileNotFoundException, IOException {
+
+    private final String line;
+
+    Solution(final String line) {
+        this.line = line;
+    }
+
+    private static Pair morphemeToPair(final String morpheme) {
+        final Pair p = new Pair();
+        final String[] spl = morpheme.split("-");
+        p.left = Integer.parseInt(spl[0]);
+        p.right = Integer.parseInt(spl[1]);
+
+        return p;
+    }
+
+    private List<Pair> lineToPairs(final String line) {
+        final String[] spl = line.split(",");
+        return Arrays.stream(spl).map(Solution::morphemeToPair).toList();
+    }
+
+    public String solve() {
+        List<Pair> pairs = lineToPairs(this.line);
+        System.out.println("Pairs:");
+        System.out.println(pairs);
+        return "TODO";
+
+    }
+
+    private static String getLine(final String fileName) throws FileNotFoundException, IOException {
         final File file = new File(fileName);
         final FileReader fr = new FileReader(file);
 
@@ -21,6 +61,10 @@ class Solution {
 
         final String input = getLine(fileName);
         System.out.println(input);
+
+        final Solution solver = new Solution(input);
+        final String answer = solver.solve();
+        System.out.println("Answer: " + answer);
 
     }
 }
