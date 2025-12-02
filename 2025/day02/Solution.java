a0ec5d71837675fe3a5c09898f8c1798dd4ea6bf
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2025-12-02 08:56:55 +0100

    Sett opp dag 2 i Java.

---
+++ 2025/day02/Solution.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+class Solution {
+    final private static String getLine(final String fileName) throws FileNotFoundException, IOException {
+        final File file = new File(fileName);
+        final FileReader fr = new FileReader(file);
+
+        final String line = fr.readAllAsString();
+        fr.close();
+        return line;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        assert args.length > 0;
+
+        final String fileName = args[0];
+        System.out.println("File name: " + fileName);
+
+        final String input = getLine(fileName);
+        System.out.println(input);
+
+    }
+}
