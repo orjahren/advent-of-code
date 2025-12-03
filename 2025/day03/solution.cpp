6282f45c000f75d9a82d34d7f6f4aa8659e302f3
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2025-12-03 07:56:22 +0100

    P1.

--- 2025/day03/solution.cpp
+++ 2025/day03/solution.cpp
@@ -2,6 +2,26 @@
 
 using namespace std;
 
+int getValue(string &bank)
+{
+    int curr = 0;
+    string cand;
+    for (int i = 0; i < bank.size(); i++)
+    {
+        for (int j = i; j < bank.size(); j++)
+        {
+            if (i == j)
+                continue;
+            // string cand = bank[i] + bank[j];
+            string cand = string(1, bank.at(i)) + bank[j];
+            int val = stoi(cand);
+            // cout << "val: " << val << endl;
+            curr = max(curr, val);
+        }
+    }
+    return curr;
+}
+
 int main()
 {
     int p1 = 0;
@@ -11,6 +31,9 @@ int main()
     {
         cout << inp << endl;
         cout << "i: " << i++ << endl;
+        int val = getValue(inp);
+        cout << "Value: " << val << endl;
+        p1 += val;
     }
     cout << "Part 1: " << p1 << endl;
 }
