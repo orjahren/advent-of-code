132f258d1bfc0f770733fc5cac5afff70cf5198c
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2025-12-03 07:50:07 +0100

    Init dag 3.

---
+++ 2025/day03/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int p1 = 0;
+    string inp;
+    int i = 0;
+    while (cin >> inp)
+    {
+        cout << inp << endl;
+        cout << "i: " << i++ << endl;
+    }
+    cout << "Part 1: " << p1 << endl;
+}
