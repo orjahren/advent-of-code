127b0968fbe7c7e04ad45b3c02f0d39b8ba970ad
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2025-12-04 20:42:23 +0100

    Bygg grid.

--- 2025/day04/Solution.java
+++ 2025/day04/Solution.java
@@ -2,8 +2,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -21,35 +19,45 @@ class Solution {
     public final static boolean DEBUG = true;
     public long p1Seq, p2Seq, p2Par;
 
-    Solution() {
+    private char[][] grid;
+    private List<String> lines;
+
+    Solution(List<String> lines) {
+        int x = lines.get(0).length();
+        int y = lines.size();
+        this.grid = new char[x][y];
+        this.lines = lines;
     }
 
-    private static Pair morphemeToPair(final String morpheme) {
-        final Pair p = new Pair();
-        final String[] spl = morpheme.split("");
-        p.left = -1;
-        p.right = -1;
-
-        return p;
-    }
-
-    private static List<Pair> lineToCoords(final String line) {
-        final String[] spl = line.split("");
-        System.out.println(spl);
-        return Arrays.stream(spl).map(Solution::morphemeToPair).toList();
-    }
-
-    private String solve(final List<String> lines) {
-        final List<Pair> coords = new ArrayList<>();
-        for (String line : lines) {
-            List<Pair> lineCoords = lineToCoords(line);
-            coords.addAll(lineCoords);
+    private void processLines() {
+        // for (String line : lines) {
+        for (int i = 0; i < lines.size(); i++) {
+            final String line = lines.get(i);
+            // final String[] spl = line.split("");
+            // TODO: Alle linjer vil ha samme lengde, kan forenkle?
+            for (int j = 0; j < line.length(); j++) {
+                final char c = line.charAt(j);
+                System.out.println("{" + i + ", " + j + "}");
+                grid[i][j] = c;
+            }
+            System.out.println();
         }
+    }
 
-        if (DEBUG)
-            System.out.println("Coords:");
-        if (DEBUG)
-            System.out.println(coords);
+    private void printGrid(char[][] grid) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                final char c = grid[i][j];
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+
+    private int solve() {
+        processLines();
+
+        printGrid(this.grid);
 
         final long startTimeSeq = System.nanoTime();
         final long seqSolution = -1;// solveSeq(pairs);
@@ -58,7 +66,7 @@ class Solution {
 
         System.out.println("Sequential execution took " + TimeUnit.NANOSECONDS.toMillis(seqDuration) + "ms");
 
-        return String.valueOf(seqSolution);
+        return (int) seqSolution;
 
     }
 
@@ -78,14 +86,10 @@ class Solution {
         System.out.println("File name: " + fileName);
 
         final String input = getLine(fileName);
-        // if (DEBUG)
-        System.out.println(input);
 
-        final Solution solution = new Solution();
-        final long p1 = solution.p1Seq;
-        final long p2 = solution.p2Par;
+        final Solution solution = new Solution(input.lines().toList());
+        final int p1 = solution.solve();
         System.out.println("Part 1: " + p1);
-        System.out.println("Part 2: " + p2);
 
     }
 }
