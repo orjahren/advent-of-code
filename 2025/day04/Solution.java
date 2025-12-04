2127479332fd52b5ef9eb1f00b4e8333bf3f8c08
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2025-12-04 21:16:06 +0100

    p1.

--- 2025/day04/Solution.java
+++ 2025/day04/Solution.java
@@ -2,6 +2,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -44,6 +46,54 @@ class Solution {
         }
     }
 
+    private boolean isInBounds(int val) {
+        return val >= 0 && val < grid.length && val < grid[0].length;
+
+    }
+
+    private List<Pair> getNeighbors(int x, int y) {
+        final List<Pair> res = new ArrayList<>();
+        final int[][] coords = new int[][] {
+                { -1, -1 }, { -1, 0 }, { -1, 1 },
+                { 0, -1 }, { 0, 1 },
+                { 1, -1 }, { 1, 0 }, { 1, 1 },
+
+        };
+        for (int i = 0; i < coords.length; i++) {
+
+            int a = coords[i][0];
+            int b = coords[i][1];
+
+            Pair p = new Pair();
+
+            p.left = x + a;
+            p.right = y + b;
+            if (isInBounds(p.left) && isInBounds(p.right)) {
+                res.add(p);
+            }
+        }
+        return res;
+    }
+
+    private boolean isElidgeble(int x, int y, int maxOccupied) {
+
+        final List<Pair> neighbors = getNeighbors(x, y);
+        System.out.println("Jeg er " + x + " , " + y + ", og dette er mine naboer: ");
+        System.out.println(neighbors);
+        int numOccupiedNeighbors = 0;
+        for (Pair p : neighbors) {
+            char other = grid[p.left][p.right];
+            System.out.print(other);
+            if (other == '@') {
+                numOccupiedNeighbors++;
+            }
+        }
+        System.out.println();
+        System.out.println("Denne har " + numOccupiedNeighbors + " occupied :o ");
+        return numOccupiedNeighbors < maxOccupied;
+
+    }
+
     private void printGrid(char[][] grid) {
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
@@ -58,15 +108,36 @@ class Solution {
         processLines();
 
         printGrid(this.grid);
-
         final long startTimeSeq = System.nanoTime();
-        final long seqSolution = -1;// solveSeq(pairs);
+
+        int seqSolution = 0;
+
+        final char[][] mutatedGrid = Arrays.copyOf(grid, grid.length);
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                final char c = grid[i][j];
+                if (c == '.') {
+                    continue;
+                }
+                // System.out.print(c);
+                System.out.println("SJekker naboer for " + c + " på pos " + i + ", " + j);
+                if (isElidgeble(i, j, 4)) {
+                    seqSolution++;
+                    // mutatedGrid[i][j] = 'x';
+                }
+            }
+            System.out.println();
+        }
+
         final long endTimeSeq = System.nanoTime();
         final long seqDuration = endTimeSeq - startTimeSeq;
 
         System.out.println("Sequential execution took " + TimeUnit.NANOSECONDS.toMillis(seqDuration) + "ms");
+        System.out.println("\nMutated grid: ");
+        printGrid(mutatedGrid);
 
-        return (int) seqSolution;
+        return seqSolution;
 
     }
 
