f141198eaebcbbfc49a416b69614f3d01bcc19ed
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2025-12-10 14:05:53 +0100

    p2: finn innhyllinga.

--- 2025/day09/solution.go
+++ 2025/day09/solution.go
@@ -42,6 +42,70 @@ func readInput(scanner *bufio.Scanner) []Pair {
 	return pairs
 }
 
+func printGrid(redTiles, greenTiles []Pair) {
+	tiles := append(append([]Pair{}, redTiles...), greenTiles...)
+	minX, maxX, minY, maxY := 0, 0, 0, 0
+	for _, t := range tiles {
+		if t.a < minX {
+			minX = t.a
+		}
+		if t.a > maxX {
+			maxX = t.a
+		}
+		if t.b < minY {
+			minY = t.b
+		}
+		if t.b > maxY {
+			maxY = t.b
+		}
+	}
+
+	width := maxX - minX + 1
+	height := maxY - minY + 1
+
+	grid := make([][]rune, height)
+	for y := range grid {
+		grid[y] = make([]rune, width)
+		for x := range grid[y] {
+			grid[y][x] = '.'
+		}
+	}
+
+	draw := func(tiles []Pair, mark rune) {
+		for _, t := range tiles {
+			grid[t.b-minY][t.a-minX] = mark
+		}
+	}
+
+	draw(greenTiles, 'G')
+	draw(redTiles, 'R')
+
+	for _, row := range grid {
+		fmt.Println(string(row))
+	}
+}
+
+func getPathBetweenCoords(from, to Pair) []Pair {
+	path := make([]Pair, 0)
+
+	startX, endX := from.a, to.a
+	if startX > endX {
+		startX, endX = endX, startX
+	}
+	startY, endY := from.b, to.b
+	if startY > endY {
+		startY, endY = endY, startY
+	}
+
+	for x := startX; x <= endX; x++ {
+		for y := startY; y <= endY; y++ {
+			path = append(path, Pair{a: x, b: y})
+		}
+	}
+
+	return path
+}
+
 func getRectangleSize(start, end Pair) int {
 	width := end.a - start.a + 1
 	height := end.b - start.b + 1
@@ -56,16 +120,31 @@ func main() {
 	fmt.Println(pairs)
 	fmt.Println("Num pairs:", len(pairs))
 
-	m := 0
+	outline := make([]Pair, 0)
+	for i, x := range pairs {
+		next := (i + 1) % len(pairs)
+		y := pairs[next]
+		if x.a == y.a {
+			// vertical line
+			path := getPathBetweenCoords(x, y)
+			outline = append(outline, path...)
+		} else if x.b == y.b {
+			// horizontal line
+			path := getPathBetweenCoords(x, y)
+			outline = append(outline, path...)
+		}
+	}
+	printGrid(pairs, outline)
+
+	p1 := 0
 	for i, x := range pairs {
 		for j, y := range pairs {
 			if i != j {
 				area := getRectangleSize(x, y)
-				m = max(m, area)
+				p1 = max(p1, area)
 			}
 		}
 	}
-	p1 := m
 
 	fmt.Println("Part 1:", p1)
 
