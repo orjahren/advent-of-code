365353f9a92172504a335f4a2d865b96263e11b5
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2025-12-11 18:24:31 +0100

    Crazy hacks. Men det funker...

--- 2025/day06/part2.py
+++ 2025/day06/part2.py
@@ -1,24 +1,46 @@
+import sys
 from typing import List
 
-
-# with open("example") as f:
-with open("input") as f:
+filename = sys.argv[1]
+with open(filename) as f:
     content = [x.strip("\n") for x in f.readlines()]
     print(content)
 
 print(content)
 
+opr_line = content[-1]
+opr_list = [x for x in opr_line.split() if x]
+content = content[:-1]
+print("opr line:", opr_line)
+print("content uten ops:", content)
+
 matrix = [[] for _ in content]
 print(matrix)
 
-WINDOW_SIZE = 4
-for line_idx, line in enumerate(content):
-    print("Line:")
-    print(line)
-    for idx in range(0, len(line), WINDOW_SIZE):
-        sub = line[idx:idx+WINDOW_SIZE]
-        print(sub)
+# for line_idx, line in enumerate(content):
+for char_idx, char in enumerate(opr_line):
+    print("opr char", char, "for idx", char_idx)
+    if (not char.strip()):
+        print("**continuer")
+        continue
+
+    next_opr_idx = char_idx+1
+    try:
+        while not opr_line[next_opr_idx].strip():
+            next_opr_idx += 1
+    except Exception:
+        next_opr_idx = len(opr_line) + 1
+
+    for line_idx, line in enumerate(content):
+        line = content[line_idx]
+        print("Line:")
+        print(line)
+        # for idx in range(char_idx, next_opr_idx):
+        # for idx in range(0, len(line), WINDOW_SIZE):
+        sub = line[char_idx:next_opr_idx]
+        print("sub:", sub)
         matrix[line_idx].append(sub)
+    old_char_idx = char_idx
 print(matrix)
 
 for row_idx, row in enumerate(matrix):
@@ -37,7 +59,11 @@ def get_nums(values: List[str]) -> List[int]:
         val = values[i]
         print(val)
         for j in range(len(values)):
-            stax[i].append(val[j])
+            try:
+                x = val[j]
+            except Exception:
+                x = ""
+            stax[i].append(x)
 
     print("stax:")
     print(stax)
@@ -53,11 +79,16 @@ def get_nums(values: List[str]) -> List[int]:
     print("comp:")
     print(comp)
 
-    try:
-
-        parsed = [int("".join(x)) for x in comp if x]
-    except Exception:
-        return [0]
+    stripped = []
+    for l in comp:
+        p = []
+        for x in l:
+            y = x.strip()
+            if y:
+                p.append(y)
+        if len(p) > 0:
+            stripped.append(p)
+    parsed = [int("".join(x)) for x in stripped if x]
 
     return parsed
 
@@ -68,9 +99,10 @@ for col_idx, _ in enumerate(matrix[0]):
     values = [row[col_idx] for row in matrix]
     print("values", col_idx, values)
 
-    opr = values[-1].strip()
-    values = values[:-1]
-    print("values uten op ", col_idx, values)
+    # opr = values[-1].strip()
+    opr = opr_list[col_idx]
+    # values = values[:-1]
+    # print("values uten op ", col_idx, values)
 
     print("op:", opr)
 
