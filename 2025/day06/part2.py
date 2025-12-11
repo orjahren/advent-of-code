335d8f748d3b121b1e26bdeb26e8242358160866
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2025-12-11 18:26:34 +0100

    Få ned linjetallet ved å fjerne div ting.

--- 2025/day06/part2.py
+++ 2025/day06/part2.py
@@ -4,24 +4,15 @@ from typing import List
 filename = sys.argv[1]
 with open(filename) as f:
     content = [x.strip("\n") for x in f.readlines()]
-    print(content)
-
-print(content)
 
 opr_line = content[-1]
 opr_list = [x for x in opr_line.split() if x]
 content = content[:-1]
-print("opr line:", opr_line)
-print("content uten ops:", content)
 
 matrix = [[] for _ in content]
-print(matrix)
 
-# for line_idx, line in enumerate(content):
 for char_idx, char in enumerate(opr_line):
-    print("opr char", char, "for idx", char_idx)
     if (not char.strip()):
-        print("**continuer")
         continue
 
     next_opr_idx = char_idx+1
@@ -33,31 +24,15 @@ for char_idx, char in enumerate(opr_line):
 
     for line_idx, line in enumerate(content):
         line = content[line_idx]
-        print("Line:")
-        print(line)
-        # for idx in range(char_idx, next_opr_idx):
-        # for idx in range(0, len(line), WINDOW_SIZE):
         sub = line[char_idx:next_opr_idx]
-        print("sub:", sub)
         matrix[line_idx].append(sub)
     old_char_idx = char_idx
-print(matrix)
-
-for row_idx, row in enumerate(matrix):
-    # print("row", row_idx, row)
-
-    for col_idx, col in enumerate(row):
-        # print("\tcol", col_idx, col)
-        pass
 
 
 def get_nums(values: List[str]) -> List[int]:
-    print("Finne nums fra values:", values)
     stax = [[] for _ in values]
-    # for i in range(WINDOW_SIZE - 1):
     for i in range(len(values)):
         val = values[i]
-        print(val)
         for j in range(len(values)):
             try:
                 x = val[j]
@@ -65,19 +40,12 @@ def get_nums(values: List[str]) -> List[int]:
                 x = ""
             stax[i].append(x)
 
-    print("stax:")
-    print(stax)
-
     comp = [[] for _ in stax]
     for i in range(len(stax)):
         for j in range(len(stax)):
             if i != j:
-                # continue
                 pass
             comp[i].append(stax[j][i])
-
-    print("comp:")
-    print(comp)
 
     stripped = []
     for l in comp:
@@ -94,18 +62,9 @@ def get_nums(values: List[str]) -> List[int]:
 
 
 p2 = 0
-
 for col_idx, _ in enumerate(matrix[0]):
     values = [row[col_idx] for row in matrix]
-    print("values", col_idx, values)
-
-    # opr = values[-1].strip()
     opr = opr_list[col_idx]
-    # values = values[:-1]
-    # print("values uten op ", col_idx, values)
-
-    print("op:", opr)
-
     nums = get_nums(values)
 
     if opr == "+":
@@ -115,8 +74,5 @@ for col_idx, _ in enumerate(matrix[0]):
         for num in nums:
             prod *= num
         p2 += prod
-    else:
-        print("Ukjent op woah hvordan kunne dette skje :o")
-        exit(1)
 
 print("p2:", p2)
